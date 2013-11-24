ada0525949d425645abc5b9237a0236f1462fb5a
Robert Strandh <robert.strandh@gmail.com>
2013-11-24 19:37:54 +0100

    Exported names of functions.

--- Basic-Emacs/packages.lisp
+++ Basic-Emacs/packages.lisp
@@ -3,4 +3,12 @@
 (defpackage #:climacs-basic-emacs
   (:use #:common-lisp)
   (:export
+   #:forward-item
+   #:backward-item
+   #:insert-item
+   #:delete-item
+   #:erase-item
+   #:item-before-cursor
+   #:item-after-cursor
+   #:buffer-from-stream
    ))
