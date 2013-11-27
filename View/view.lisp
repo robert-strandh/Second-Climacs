31f708b53d77737ca5be26456dd9fc3af1867b30
Robert Strandh <robert.strandh@gmail.com>
2013-11-27 12:39:27 +0100

    New subdirectory for views.

---
+++ View/view.lisp
@@ -0,0 +1,30 @@
+(defpackage #:climacs-view
+  (:use #:common-lisp)
+  (:export
+   #:analyzer
+   #:show
+   #:command-table
+   #:command-key-processor
+   #:cursor
+   #:view
+   ))
+
+(in-package #:climacs-view)
+
+(defclass view ()
+  ((%analyzer :initarg :analyzer :reader analyzer)
+   (%show :initarg :show :accessor show)
+   (%command-key-processor
+    :initarg :command-key-processor
+    :reader clim3:command-table
+    :accessor command-key-processor)
+   (%cursor :initarg :cursor :reader cursor)))
+
+(defmethod clim3:acquire-action :around ((view view))
+  (let ((clim3:*key-handler*
+	  (make-instance 'clim3:key-processor-key-handler
+	    :processor (command-key-processor view))))
+    (call-next-method)))
+
+  
+
