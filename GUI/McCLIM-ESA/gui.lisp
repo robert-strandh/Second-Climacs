7d2e662acdf85b45c905c18d40c57555eff82fe2
Robert Strandh <robert.strandh@gmail.com>
2015-11-17 11:08:02 +0100

    Define class for Climacs application frame.

--- GUI/McCLIM-ESA/gui.lisp
+++ GUI/McCLIM-ESA/gui.lisp
@@ -1,1 +1,5 @@
 (cl:in-package #:climacs-esa-gui)
+
+(defclass climacs (clim:application-frame esa:esa-frame-mixin)
+  ((%views :initarg :views :reader views)
+   (%current-view :initarg :current-view :accessor current-view)))
