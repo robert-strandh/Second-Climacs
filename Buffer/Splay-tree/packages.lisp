36621cbdcea64b9d8bd1932e0bd446082a6d2217
Robert Strandh <robert.strandh@gmail.com>
2015-11-18 16:06:29 +0100

    Add a comment for each exported symbol.

--- Buffer/Splay-tree/packages.lisp
+++ Buffer/Splay-tree/packages.lisp
@@ -3,8 +3,15 @@
 (defpackage #:splay-tree
   (:use #:common-lisp)
   (:export
+   ;; The name of the class for nodes of the splay tree.
    #:node
+   ;; The name of the operation for splaying a node.
    #:splay
+   ;; The name of the accessor for obtaining or changing the left
+   ;; child of a node.
    #:left
+   ;; The name of the accessor obtaining or changing the right child
+   ;; of a node.
    #:right
+   ;; The name of a generic function for obtaining the parent of a
    #:parent))
