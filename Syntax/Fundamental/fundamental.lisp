4862bf010b7f76fd1fab92afcc2d6c3f12eaf93b
Robert Strandh <robert.strandh@gmail.com>
2015-12-15 10:14:17 +0100

    Call UPDATE in Cluffer library, and re-indent modified line.

--- Syntax/Fundamental/fundamental.lisp
+++ Syntax/Fundamental/fundamental.lisp
@@ -162,12 +162,12 @@
 		   do (delete-node node))
 	     (touch (splay (find-node current-line-number analyzer)))
 	     (incf current-line-number)))
-      (climacs-buffer:update (buffer analyzer)
-			     (buffer-time analyzer)
-			     #'sync #'skip #'modify #'create)))
+      (cluffer:update (buffer analyzer)
+		      (buffer-time analyzer)
+		      #'sync #'skip #'modify #'create)))
   ;; Record the time at which this update was made.
   (setf (buffer-time analyzer)
-	(climacs-buffer:current-time (buffer analyzer))))
+	(cluffer:current-time (buffer analyzer))))
 
 (defun synchronize (analyzer time sync skip modify create)
   (update analyzer)
