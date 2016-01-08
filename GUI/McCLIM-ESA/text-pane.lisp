18ec15b3e41389331bb76c63aa6928d3b4238b9a
Robert Strandh <robert.strandh@gmail.com>
2016-01-08 15:13:31 +0100

    Define class for new pane.

--- GUI/McCLIM-ESA/text-pane.lisp
+++ GUI/McCLIM-ESA/text-pane.lisp
@@ -1,2 +1,8 @@
 (cl:in-package #:climacs-esa-gui)
 
+(defclass text-pane (climi::composite-pane
+		     clim:permanent-medium-sheet-output-mixin)
+  ((%lines :initarg :lines :accessor lines))
+  (:default-initargs
+   :background clim:+white+
+   :text-style (clim:make-text-style :fixed nil 14)))
