f1771e4843003fb273461ceccbe1316d3f3895da
Robert Strandh <robert.strandh@gmail.com>
2016-03-25 10:05:25 +0100

    Add comment explaning how we get around absolute record positions.

--- GUI/McCLIM-ESA/Simple-record/record.lisp
+++ GUI/McCLIM-ESA/Simple-record/record.lisp
@@ -5,3 +5,30 @@
    ;; of child output records (lines).  If there are no children, then
    ;; this slot contains NIL.
    (%contents :initform nil :initarg :contents :accessor contents)))
+
+;;; The CLIM II specification requires the position of an output
+;;; record to be relative to the stream in which it is located, rather
+;;; than to its parent.  We interpret that to mean that the following
+;;; generic functions accept or return stream-relative coordinates:
+;;;
+;;;  - output-record-position
+;;;  - (setf* output-record-position)
+;;;  - output-record-start-cursor-position
+;;;  - (setf* output-record-start-cursor-position)
+;;;  - output-record-end-cursor-position
+;;;  - (setf* output-record-end-cursor-position)
+;;;  - replay-output-record
+;;;  - output-record-hit-detection-rectangle*
+;;;  - output-record-refined-position-test
+;;;
+;;; Clearly, we do not want to store stream-relative positions in the
+;;; output records for individual lines, because then we would have to
+;;; update all following such records when one is inserted or deleted.
+;;; We get around this problem by requiring that the kind of child
+;;; output records that can be contained in this top-level record be
+;;; subclasses of RELATIVE-COORDINATES-OUTPUT-RECORD-MIXIN.
+;;;
+;;; In order for this scheme to work, we have to rely on CLIM not
+;;; caching the position of our relative output records, because those
+;;; positions may change as a result of editing operations, and
+;;; without (SETF* OUTPUT-RECORD-POSITION) being called.
