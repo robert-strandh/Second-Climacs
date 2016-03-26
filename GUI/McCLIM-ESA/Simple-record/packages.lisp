72f5cc2639403940083eebe3dac9f81ea00b1421
Robert Strandh <robert.strandh@gmail.com>
2016-03-26 11:05:33 +0100

    Export name of new output record class.

--- GUI/McCLIM-ESA/Simple-record/packages.lisp
+++ GUI/McCLIM-ESA/Simple-record/packages.lisp
@@ -2,4 +2,5 @@
 
 (defpackage #:clim-simple-editor-record
   (:use #:common-lisp)
-  (:export #:relative-coordinates-output-record-mixin))
+  (:export #:relative-coordinates-output-record-mixin
+	   #:record))
