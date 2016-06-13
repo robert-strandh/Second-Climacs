f125098e4abeb03495461f5f3400b37eea0b3d99
Robert Strandh <robert.strandh@gmail.com>
2016-06-13 15:55:06 +0200

    Remove STANDARD-TREE-OUTPUT-HISTORY as a superclass.

--- GUI/McCLIM-ESA/List-output-history/list-output-history.lisp
+++ GUI/McCLIM-ESA/List-output-history/list-output-history.lisp
@@ -1,6 +1,8 @@
 (cl:in-package #:climacs-list-output-history)
 
-(defclass list-output-history (clim:standard-tree-output-history)
-  ((%lines :initform (list nil) :reader lines)
+(defclass list-output-history
+    (clim:output-record clim:stream-output-history-mixin)
+  ((%parent :initarg :parent :reader clim:output-record-parent)
+   (%lines :initform (list nil) :reader lines)
    (%time-stamp :initform nil :accessor time-stamp)
    (%buffer :initarg :buffer :reader buffer)))
