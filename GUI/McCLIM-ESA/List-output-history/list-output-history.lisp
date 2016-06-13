eb3d725ede28ec0c4e6f42c276fe5158e36be33a
Robert Strandh <robert.strandh@gmail.com>
2016-06-13 16:01:38 +0200

    Define method on REPLAY-OUTPUT-RECORD for our history.

--- GUI/McCLIM-ESA/List-output-history/list-output-history.lisp
+++ GUI/McCLIM-ESA/List-output-history/list-output-history.lisp
@@ -6,3 +6,12 @@
    (%lines :initform (list nil) :reader lines)
    (%time-stamp :initform nil :accessor time-stamp)
    (%buffer :initarg :buffer :reader buffer)))
+
+(defmethod clim:replay-output-record
+    ((record list-output-history) stream &optional region x-offset y-offset)
+  (declare (ignore x-offset y-offset))
+  (loop for record in (cdr (lines record))
+	for height = (clim:bounding-rectangle-height record)
+	for y = 0 then (+ y height 5)
+	do (setf (clim:output-record-position record) (values 0 y))
+	   (clim:replay-output-record record stream region)))
