d00f544925261deb7f6d0f84fd8740a1e67040c2
Robert Strandh <robert.strandh@gmail.com>
2016-07-13 11:09:45 +0200

    Define a function for splitting a lambda list into required and optional.

--- Command/command.lisp
+++ Command/command.lisp
@@ -3,3 +3,11 @@
 (defclass command (standard-generic-function)
   ((%lambda-list :initarg :lambda-list :reader lambda-list))
   (:metaclass #.(class-name (class-of (find-class 'standard-generic-function)))))
+
+(defun split-lambda-list (lambda-list)
+  (let* ((position-or-nil (position '&optional lambda-list))
+	 (position (if (null position-or-nil)
+		       (length lambda-list)
+		       position-or-nil)))
+    (values (subseq lambda-list 0 position)
+	    (subseq lambda-list position))))
