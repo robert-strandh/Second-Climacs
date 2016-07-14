6689bd6782f8eeeebff70a3533d8753a73a0124c
Robert Strandh <robert.strandh@gmail.com>
2016-07-14 16:25:02 +0200

    Define generic function INVOKE and methods specialized to invocations.

--- Command/invocation.lisp
+++ Command/invocation.lisp
@@ -11,3 +11,30 @@
 
 (defclass pass-gesture-as-argument (invocation)
   ())
+
+(defgeneric invoke (command invocation gesture numeric-arg numeric-arg-p))
+
+(defmethod invoke (command
+		   (invocation pass-numeric-argument-as-argument)
+		   gesture
+		   numeric-arg
+		   numeric-arg-p)
+  (declare (ignore gesture numeric-arg-p))
+  (invoke-command command (list numeric-arg)))
+
+(defmethod invoke (command
+		   (invocation repeat-according-to-numeric-argument)
+		   gesture
+		   numeric-arg
+		   numeric-arg-p)
+  (declare (ignore gesture numeric-arg-p))
+  (loop repeat numeric-arg
+	do (invoke-command command '())))
+
+(defmethod invoke (command
+		   (invocation pass-gesture-as-argument)
+		   gesture
+		   numeric-arg
+		   numeric-arg-p)
+  (declare (ignore numeric-arg numeric-arg-p))
+  (invoke-command command (list gesture)))
