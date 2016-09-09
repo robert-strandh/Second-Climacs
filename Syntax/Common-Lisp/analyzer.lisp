f573fd79a8343a5004f20ca210696d111fda08e7
Robert Strandh <robert.strandh@gmail.com>
2016-09-09 15:07:42 +0200

    Define function PREFIX-TO-SUFFIX.

--- Syntax/Common-Lisp/analyzer.lisp
+++ Syntax/Common-Lisp/analyzer.lisp
@@ -19,15 +19,33 @@
 		   (suffix suffix))
       analyzer
     (assert (not (null suffix)))
-    (let* ((first-cons-cell prefix)
+    (let* ((first-cons-cell suffix)
 	   (first-parse-result (first first-cons-cell)))
       (setf suffix (rest suffix))
       (unless (null suffix)
 	;; Convert start-line of new first element of suffix
 	;; to absolute line number by adding the absolute line
-	;; number of element we are removing.
+	;; number of the element we are removing.
 	(incf (start-line (first suffix))
 	      (start-line first-parse-result)))
       ;; Reuse the CONS cell rather than allocating a new one.
       (setf (rest first-cons-cell) prefix)
       (setf prefix first-cons-cell))))
+
+(defun prefix-to-suffix (analyzer)
+  (with-accessors ((prefix prefix)
+		   (suffix suffix))
+      analyzer
+    (assert (not (null prefix)))
+    (let* ((first-cons-cell prefix)
+	   (first-parse-result (first first-cons-cell)))
+      (setf prefix (rest prefix))
+      (unless (null suffix)
+	;; Convert start-line of current first element of suffix to
+	;; relative line number by subtracting the absolute line
+	;; number of the element we are adding.
+	(decf (start-line (first suffix))
+	      (start-line first-parse-result)))
+      ;; Reuse the CONS cell rather than allocating a new one.
+      (setf (rest first-cons-cell) suffix)
+      (setf suffix first-cons-cell))))
