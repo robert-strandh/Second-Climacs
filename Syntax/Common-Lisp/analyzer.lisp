a7c547d3bd76bdd3deefd750f0c6d766db5c0bd9
Robert Strandh <robert.strandh@gmail.com>
2016-09-09 14:50:29 +0200

    Define class PARSE-RESULT.

--- Syntax/Common-Lisp/analyzer.lisp
+++ Syntax/Common-Lisp/analyzer.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
+(defclass parse-result ()
+  ((%start-line :initarg :start-line :accessor start-line)
+   (%end-line :initarg :end-line :accessor end-line)
+   (%start-column :initarg :start-column :accessor start-column)
+   (%end-column :initarg :end-column :accessor end-column)
+   (%children :initarg :children :accessor children)
+   (%expression :initarg expression :accessor expression)))
+
 (defclass analyzer ()
   ((%prefix :initform '() :accessor prefix)
    (%suffix :initform '() :accessor suffix)
