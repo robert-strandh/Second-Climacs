16ba8464fa676bca336ffaa45aa1b1ade9156088
Robert Strandh <robert.strandh@gmail.com>
2016-09-27 07:13:01 +0200

    Add comments explaining the definitions.

--- Syntax/Common-Lisp/folio.lisp
+++ Syntax/Common-Lisp/folio.lisp
@@ -1,11 +1,23 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
+;;; This file contains the definition of a protocol called FOLIO.
+;;; Conceptually, a folio consists of a certain number of LINEs, each
+;;; line containing a certain number of ITEMs.  Lines are numbered
+;;; sequentially starting at 0 within a folio, and items are numbered
+;;; sequentially starting at 0 within each line.
+
+;;; The base class for all folios.
 (defclass folio () ())
 
+;;; Given a folio, return the number of lines contained in the folio.
 (defgeneric line-count (folio))
 
+;;; Given a folio and a line number, return the number of items in the
+;;; line with that line number.
 (defgeneric line-length (folio line-number))
 
 (defgeneric line-items (folio line-number))
 
+;;; Given a folio, a line number an item number within that line,
+;;; return the item at that position in that line.
 (defgeneric item (folio line-number column-number))
