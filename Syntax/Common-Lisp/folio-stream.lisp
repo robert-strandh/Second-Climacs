800baa3fe75225573d9f32ce6ddf61ec8ee16981
Robert Strandh <robert.strandh@gmail.com>
2016-09-28 09:42:57 +0200

    Define new class FOLIO-STREAM.

--- Syntax/Common-Lisp/folio-stream.lisp
+++ Syntax/Common-Lisp/folio-stream.lisp
@@ -1,11 +1,18 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
+;;; A folio stream is a stream that uses a folio as a source for the
+;;; items to return as a result of reading.
+(defclass folio-stream ()
+  ((%folio :initarg :folio :reader folio)
+   (%current-line-number :initform 0 :accessor current-line-number)
+   (%current-item-number :initform 0 :accessor current-item-number)))
+
 (defgeneric next-position (folio line-number item-number))
 
 (defmethod next-position ((folio folio) line-number item-number)
   (if (= (line-length folio line-number) item-number)
       (values (1+ line-number) 0)
-      (values line-number (1+ column-number))))
+      (values line-number (1+ item-number))))
 
 (defgeneric previous-position (folio line-number item-number))
 
