09f89dfa5c99564a7574b31ad4e25c3a16c7582c
Robert Strandh <robert.strandh@gmail.com>
2016-09-28 09:42:19 +0200

    Remove obsolete function EOF-P.

--- Syntax/Common-Lisp/stream.lisp
+++ Syntax/Common-Lisp/stream.lisp
@@ -5,14 +5,6 @@
    (%current-line :initform 0 :accessor current-line)
    (%current-column :initform 0 :accessor current-column)))
 
-(defun eof-p (analyzer-stream)
-  (let* ((lines (lines (analyzer analyzer-stream)))
-	 (count (flexichain:nb-elements lines)))
-    (and (= (current-line analyzer-stream) (1- count))
-	 (= (current-column analyzer-stream)
-	    (let ((last-line (flexichain:element* lines (1- count))))
-	      (length (contents last-line)))))))
-
 (defun forward (analyzer-stream)
   (with-accessors ((analyzer analyzer)
 		   (current-line current-line)
