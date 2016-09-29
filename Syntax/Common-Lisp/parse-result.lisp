c1a41cfb903e01586c5e32bfaf801ec7e03baf8a
Robert Strandh <robert.strandh@gmail.com>
2016-09-29 16:05:21 +0200

    Define subclasses of class PARSE-RESULT.

--- Syntax/Common-Lisp/parse-result.lisp
+++ Syntax/Common-Lisp/parse-result.lisp
@@ -5,6 +5,13 @@
    (%end-line :initarg :end-line :accessor end-line)
    (%start-column :initarg :start-column :accessor start-column)
    (%end-column :initarg :end-column :accessor end-column)
-   (%children :initarg :children :accessor children)
-   (%expression :initarg :expression :accessor expression)))
+   (%children :initarg :children :accessor children)))
 
+(defclass expression-parse-result (parse-result)
+  ((%expression :initarg :expression :accessor expression)))
+
+(defclass no-expression-parse-result (parse-result)
+  ())
+
+(defclass error-parse-result (parse-result)
+  ())
