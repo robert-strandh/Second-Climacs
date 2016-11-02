410a9b1e12c8e2728048eaa4440e88b2ef9dec5b
Robert Strandh <robert.strandh@gmail.com>
2016-11-02 10:31:39 +0100

    Define method on CLUFFER:UPDATE specialized to test buffer.

--- Syntax/Common-Lisp/Test/buffer.lisp
+++ Syntax/Common-Lisp/Test/buffer.lisp
@@ -15,3 +15,14 @@
 		 for items in lines
 		 collect (make-instance 'line
 			   :items items))))
+
+(defmethod cluffer:update ((buffer buffer) time sync skip modify create)
+  (let* ((line-count (length (lines buffer)))
+	 (modified-line-number (random line-count)))
+    (unless (zerop modified-line-number)
+      (funcall skip modified-line-number))
+    (funcall modify (nth modified-line-number (lines buffer)))
+    (unless (= modified-line-number (1- line-count))
+      (funcall sync (nth (1+ modified-line-number) (lines buffer))))
+    (unless (= modified-line-number (- line-count 2))
+      (funcall skip (- line-count modified-line-number 2)))))
