61707cdbc20efdb044ac1b91eafc23727ed49c18
Robert Strandh <robert.strandh@gmail.com>
2016-11-02 08:18:42 +0100

    New function for returning a random symbol.

--- Syntax/Common-Lisp/Test/random-form.lisp
+++ Syntax/Common-Lisp/Test/random-form.lisp
@@ -5,3 +5,8 @@
 
 (defun random-character ()
   (code-char (+ 32 (random 96))))
+
+(defun random-symbol ()
+  (loop for sym being each external-symbol in '#:common-lisp
+	repeat (random 500)
+	finally (return sym)))
