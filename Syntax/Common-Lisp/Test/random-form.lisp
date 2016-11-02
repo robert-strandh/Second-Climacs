8795825ec3a6a65e200a7cc93e9cff69520d2d5d
Robert Strandh <robert.strandh@gmail.com>
2016-11-02 08:20:33 +0100

    New function for returning a random string.

--- Syntax/Common-Lisp/Test/random-form.lisp
+++ Syntax/Common-Lisp/Test/random-form.lisp
@@ -10,3 +10,8 @@
   (loop for sym being each external-symbol in '#:common-lisp
 	repeat (random 500)
 	finally (return sym)))
+
+(defun random-string ()
+  (with-output-to-string (var)
+    (loop repeat (random 10)
+	  do (write-char (random-character) var))))
