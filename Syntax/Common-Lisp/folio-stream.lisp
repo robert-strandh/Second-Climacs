523af5f9a4778a401d5c7439fb1a713489c39001
Robert Strandh <robert.strandh@gmail.com>
2016-11-03 11:44:19 +0100

    Move to previous line when ITEM-NUMBER is 0.

--- Syntax/Common-Lisp/folio-stream.lisp
+++ Syntax/Common-Lisp/folio-stream.lisp
@@ -18,7 +18,7 @@
 
 (defmethod previous-position ((folio folio) line-number item-number)
   (if (zerop item-number)
-      (values 0 (line-length folio (1- line-number)))
+      (values (1- line-number) (line-length folio (1- line-number)))
       (values line-number (1- item-number))))
 
 (defgeneric eof-p (folio-stream))
