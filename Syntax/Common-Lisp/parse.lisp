567dad72dd054f9357b2d5734e968e61fb6e4af4
Robert Strandh <robert.strandh@gmail.com>
2016-11-04 14:58:43 +0100

    Define function for parsing entire buffer.

--- Syntax/Common-Lisp/parse.lisp
+++ Syntax/Common-Lisp/parse.lisp
@@ -9,3 +9,16 @@
   (push (parse analyzer-stream)
 	(prefix (folio analyzer-stream))))
 
+(defun parse-buffer (analyzer-stream)
+  (loop with analyzer = (folio analyzer-stream)
+	until (progn
+		(skip-whitespace analyzer-stream)
+		(or (eof-p analyzer-stream)
+		    (and (not (null (suffix analyzer)))
+			 (let* ((pr (first (suffix analyzer)))
+				(l (start-line pr))
+				(c (start-column pr)))
+			   (and (= l (current-line-number analyzer-stream))
+				(= c (current-item-number analyzer-stream)))))))
+	do (parse-and-cache analyzer-stream)
+	   (pop-to-stream-position analyzer-stream)))
