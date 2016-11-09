19adb8329d2bdf6ff04fe106c8918a4ee27ad668
Robert Strandh <robert.strandh@gmail.com>
2016-11-09 07:58:22 +0100

    Fix typo in comment.

--- Syntax/Common-Lisp/Test/cache-invalidation.lisp
+++ Syntax/Common-Lisp/Test/cache-invalidation.lisp
@@ -2,8 +2,8 @@
 
 ;;; We define a simplified version of the parse result.  It is
 ;;; simplified in that it always has absolute line numbers in it, and
-;;; there are not column numbers, since they are not used or altered
-;;; by the invalidation protocol.
+;;; there are no column numbers, since they are not used or altered by
+;;; the invalidation protocol.
 
 (defclass node ()
   ((%start-line :initarg :start-line :accessor start-line)
