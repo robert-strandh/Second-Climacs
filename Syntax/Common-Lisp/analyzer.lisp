22c2749d681c86ba4433bf70b9aa190fc2718edb
Robert Strandh <robert.strandh@gmail.com>
2016-11-10 18:17:12 +0100

    Make class ANALYZER a subclass of FLEXICHAIN-FOLIO.

--- Syntax/Common-Lisp/analyzer.lisp
+++ Syntax/Common-Lisp/analyzer.lisp
@@ -11,7 +11,7 @@
   ((%cluffer-line :initarg :cluffer-line :reader cluffer-line)
    (%contents :initarg :contents :accessor contents)))
 
-(defclass analyzer (folio)
+(defclass analyzer (flexichain-folio)
   ((%prefix :initform '() :accessor prefix)
    (%suffix :initform '() :accessor suffix)
    (%residue :initform '() :accessor residue)
