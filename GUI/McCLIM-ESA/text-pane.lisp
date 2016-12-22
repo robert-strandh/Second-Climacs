409ca85dc112e33ac767c8aec45bb1044a1dc97a
Robert Strandh <robert.strandh@gmail.com>
2016-12-22 21:33:01 +0100

    Remove incorrect method.

--- GUI/McCLIM-ESA/text-pane.lisp
+++ GUI/McCLIM-ESA/text-pane.lisp
@@ -23,29 +23,3 @@
     (clim:make-space-requirement
      :width 200
      :height (+ (* dist line-count) ascent descent))))
-
-(defmethod clim:handle-repaint ((pane text-pane) region)
-  (let* ((style (clim:pane-text-style pane))
-	 (ascent (ceiling (clim:text-style-ascent style pane)))
-	 (descent (ceiling (clim:text-style-descent style pane)))
-	 (height (clim:text-style-height style pane))
-	 (dist (ceiling (* height (+ 1.0 *base-line-extra*))))
-	 (line-count (length (lines pane))))
-    ;; Line i is drawn with its base-line at
-    ;; (+ ascent (* dist i))
-    ;;
-    ;; The first visible line is the first one drawn at a vertical
-    ;; position that is greater than or equal to (- top descent).
-    ;;
-    ;; The last visible line is the last one drawn at a vertical
-    ;; position that is less than or equal to (+ bottom ascent).
-    (clim:with-bounding-rectangle* (left top right bottom) region
-      (declare (ignore left right))
-      (let ((first-line (max 0 (floor (/ (- top descent ascent) dist))))
-	    (last-line (min (1- line-count) (ceiling (/ bottom dist)))))
-	;; FIXME: make sure those lines exist in the vector
-	(loop for i from first-line to last-line
-	      do (clim:draw-text* pane
-				  (aref (lines pane) i)
-				  0
-				  (+ ascent (* dist i))))))))
