803affd58b69bc8242550416bcaf7c94cf7fa3f1
Robert Strandh <robert.strandh@gmail.com>
2016-12-27 06:50:04 +0100

    Remove obsolete code.

--- GUI/McCLIM-ESA/text-pane.lisp
+++ GUI/McCLIM-ESA/text-pane.lisp
@@ -6,20 +6,3 @@
   (:default-initargs
    :background clim:+white+
    :text-style (clim:make-text-style :fixed nil 14)))
-
-;;; The value of this variable is a fraction of the text height to be
-;;; added to that height in order to obtain the distance between two
-;;; base lines.
-(defparameter *base-line-extra* 0.1)
-
-(defmethod clim:compose-space ((pane text-pane) &key width height)
-  (declare (ignore width height))
-  (let* ((style (clim:pane-text-style pane))
-	 (ascent (ceiling (clim:text-style-ascent style pane)))
-	 (descent (ceiling (clim:text-style-descent style pane)))
-	 (height (clim:text-style-height style pane))
-	 (dist (ceiling (* height (+ 1.0 *base-line-extra*))))
-	 (line-count (length (lines pane))))
-    (clim:make-space-requirement
-     :width 200
-     :height (+ (* dist line-count) ascent descent))))
