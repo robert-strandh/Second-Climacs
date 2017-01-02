cfd3c8bb335b60a8d79cca38fe02e473a54f6908
Robert Strandh <robert.strandh@gmail.com>
2017-01-02 15:18:36 +0100

    Define command for setting mode to Common Lisp.

--- GUI/McCLIM-ESA/global-command-table.lisp
+++ GUI/McCLIM-ESA/global-command-table.lisp
@@ -7,3 +7,11 @@
 
 (clim:define-command (com-inspect :name t :command-table global-table) ()
   (clouseau:inspector clim:*application-frame*))
+
+(clim:define-command
+    (com-common-lisp-mode :name t :command-table global-table)
+    ()
+  (let* ((current-window (esa:current-window))
+	 (clim-view (clim:stream-default-view current-window))
+	 (climacs-view (climacs-view clim-view)))
+    (climacs-syntax-common-lisp:set-common-lisp-mode climacs-view)))
