9800e694715c989a1c61f846ab0154614ebc2108
Robert Strandh <robert.strandh@gmail.com>
2017-01-05 15:53:36 +0100

    Define command ERASE-ITEM bound to Backspace.

--- GUI/McCLIM-ESA/delete-table.lisp
+++ GUI/McCLIM-ESA/delete-table.lisp
@@ -13,3 +13,15 @@
 (esa:set-key `(com-delete-item #\Newline)
 	     'delete-table
 	     '((#\d :control)))
+
+(clim:define-command
+    (com-erase-item :name t :command-table delete-table)
+    ()
+  (let* ((clim-view (clim:stream-default-view (esa:current-window)))
+	 (climacs-view (climacs-view clim-view))
+	 (cursor (climacs2-base:cursor climacs-view)))
+    (climacs2-base:erase-item cursor)))
+
+(esa:set-key `(com-erase-item #\Newline)
+	     'delete-table
+	     '((#\Backspace)))
