d7060f8cc70bc162beb9ac332d05fd594c635ee0
Robert Strandh <robert.strandh@gmail.com>
2017-01-06 13:22:03 +0100

    Define command KILL-LINE bound to C-k.

--- GUI/McCLIM-ESA/delete-table.lisp
+++ GUI/McCLIM-ESA/delete-table.lisp
@@ -10,7 +10,7 @@
 	 (cursor (climacs2-base:cursor climacs-view)))
     (climacs2-base:delete-item cursor)))
 
-(esa:set-key `(com-delete-item #\Newline)
+(esa:set-key `(com-delete-item)
 	     'delete-table
 	     '((#\d :control)))
 
@@ -22,6 +22,21 @@
 	 (cursor (climacs2-base:cursor climacs-view)))
     (climacs2-base:erase-item cursor)))
 
-(esa:set-key `(com-erase-item #\Newline)
+(esa:set-key `(com-erase-item)
 	     'delete-table
 	     '((#\Backspace)))
+
+(clim:define-command
+    (com-kill-line :name t :command-table delete-table)
+    ()
+  (let* ((clim-view (clim:stream-default-view (esa:current-window)))
+	 (climacs-view (climacs-view clim-view))
+	 (cursor (climacs2-base:cursor climacs-view)))
+    (if (cluffer:end-of-line-p cursor)
+	(climacs2-base:delete-item cursor)
+	(loop until (cluffer:end-of-line-p cursor)
+	      do (climacs2-base:delete-item cursor)))))
+
+(esa:set-key `(com-kill-line)
+	     'delete-table
+	     '((#\k :control)))
