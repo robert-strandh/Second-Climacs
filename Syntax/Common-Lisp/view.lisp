ea3555ab9bb0ee2c8873e0d593fb7e7e9b27cca6
Robert Strandh <robert.strandh@gmail.com>
2017-01-22 18:00:43 +0100

    Rename the class.

--- Syntax/Common-Lisp/view.lisp
+++ Syntax/Common-Lisp/view.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-(defclass common-lisp-view (climacs2-base:view) ())
+(defclass view (climacs2-base:view) ())
 
-(defmethod initialize-instance :after ((instance common-lisp-view) &key)
+(defmethod initialize-instance :after ((instance view) &key)
   (multiple-value-bind (buffer cursor)
       (climacs2-base:make-empty-standard-buffer-and-cursor)
     (let* ((cache (make-instance 'cache))
