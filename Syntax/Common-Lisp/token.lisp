466b3b9f2ad8dc14e0b20065623faea6c41d73b0
Robert Strandh <robert.strandh@gmail.com>
2017-02-02 13:18:50 +0100

    Add a slot for token contents.

--- Syntax/Common-Lisp/token.lisp
+++ Syntax/Common-Lisp/token.lisp
@@ -1,4 +1,8 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
 (defclass token ()
-  ())
+  ((%characters :initarg :characters :reader characters)))
+
+(defmethod print-object ((object token) stream)
+  (print-unreadable-object (object stream)
+    (format stream "~a" (characters object))))
