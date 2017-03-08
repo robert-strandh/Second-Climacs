445f7b4968ee571c3053a1621992234c386101da
Robert Strandh <robert.strandh@gmail.com>
2017-03-08 14:18:20 +0100

    Define method on CLIM:STREAM-OUTPUT-HISTORY providing indrection for Climacs views.

--- GUI/McCLIM-ESA/text-pane.lisp
+++ GUI/McCLIM-ESA/text-pane.lisp
@@ -6,3 +6,9 @@
   (:default-initargs
    :background clim:+white+
    :text-style (clim:make-text-style :fixed nil 14)))
+
+(defmethod clim:stream-output-history ((stream text-pane))
+  (let ((view (clim:stream-default-view stream)))
+    (if (typep view 'climacs-clim-view)
+        (output-history (clim:stream-default-view stream))
+        (call-next-method))))
