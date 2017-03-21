c0457f611634862c30774415303bac862f967aa3
Robert Strandh <robert.strandh@gmail.com>
2017-03-21 15:51:03 +0100

    Add comments to the slots.

--- Syntax/Fundamental/cache.lisp
+++ Syntax/Fundamental/cache.lisp
@@ -1,7 +1,11 @@
 (cl:in-package #:climacs-syntax-fundamental)
 
 (defclass entry ()
-  ((%line :initarg :line :reader line)
+  (;; This slot contains the Cluffer line object so that we can
+   ;; find it when we process a SYNC operation.
+   (%line :initarg :line :reader line)
+   ;; This slot contains the contents of the line as it was when we
+   ;; last updated the cache.
    (%contents :initarg :contents :reader contents)))
 
 (defclass cache ()
