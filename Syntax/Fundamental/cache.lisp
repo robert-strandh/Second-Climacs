e8e01f4bf5f95213a66c98073e784dcae992eb89
Robert Strandh <robert.strandh@gmail.com>
2017-03-21 17:06:35 +0100

    Make the CONTENTS slot writeable.

--- Syntax/Fundamental/cache.lisp
+++ Syntax/Fundamental/cache.lisp
@@ -12,7 +12,7 @@
    (%line :initarg :line :reader line)
    ;; This slot contains the contents of the line as it was when we
    ;; last updated the cache.
-   (%contents :initarg :contents :reader contents)
+   (%contents :initarg :contents :accessor contents)
    ;; This slot contains the number of entries in the list (prefix or
    ;; suffix) that this entry is a member of.
    (%list-length :initarg list-length :accessor list-length)))
