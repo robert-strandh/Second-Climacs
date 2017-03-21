d97ae1b3c3e74c8d00888bca61de49d2bfc9b0a5
Robert Strandh <robert.strandh@gmail.com>
2017-03-21 16:02:31 +0100

    Define generic functions and methods for pushing/popping to/from prefix/suffix.

--- Syntax/Fundamental/cache.lisp
+++ Syntax/Fundamental/cache.lisp
@@ -9,8 +9,42 @@
    (%contents :initarg :contents :reader contents)
    ;; This slot contains the number of entries in the list (prefix or
    ;; suffix) that this entry is a member of.
-   (%list-length :initarg list-length accessor list-length)))
+   (%list-length :initarg list-length :accessor list-length)))
 
 (defclass cache ()
   ((%prefix :initform () :accessor prefix)
    (%suffix :initform () :accessor suffix)))
+
+(defgeneric push-to-prefix (cache entry)
+  (:method ((cache cache) (entry entry))
+    (with-accessors ((prefix prefix)) cache
+      (setf (list-length entry)
+            (1+ (list-length (first prefix))))
+      (push entry prefix))))
+
+(defgeneric pop-from-prefix (cache)
+  (:method ((cache cache))
+    (with-accessors ((prefix prefix)) cache
+      (assert (not (null prefix)))
+      (pop prefix))))
+
+(defgeneric push-to-suffix (cache entry)
+  (:method ((cache cache) (entry entry))
+    (with-accessors ((suffix suffix)) cache
+      (setf (list-length entry)
+            (1+ (list-length (first suffix))))
+      (push entry suffix))))
+
+(defgeneric pop-from-suffix (cache)
+  (:method ((cache cache))
+    (with-accessors ((suffix suffix)) cache
+      (assert (not (null suffix)))
+      (pop suffix))))
+
+(defgeneric prefix-to-suffix (cache)
+  (:method ((cache cache))
+    (push-to-suffix cache (pop-from-prefix cache))))
+
+(defgeneric suffix-to-prefix (cache)
+  (:method ((cache cache))
+    (push-to-prefix cache (pop-from-suffix cache))))
