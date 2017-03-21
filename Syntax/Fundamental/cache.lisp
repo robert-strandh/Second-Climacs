6fc2c6a8babd30d703285378ca834a8a2dcce328
Robert Strandh <robert.strandh@gmail.com>
2017-03-21 09:21:04 +0100

    Define class ENTRY for elements of prefix and suffix.

--- Syntax/Fundamental/cache.lisp
+++ Syntax/Fundamental/cache.lisp
@@ -1,7 +1,9 @@
 (cl:in-package #:climacs-syntax-fundamental)
 
+(defclass entry ()
+  ((%line :initarg :line :reader line)
+   (%contents :initarg :contents :reader contents)))
+
 (defclass cache ()
   ((%prefix :initform () :accessor prefix)
    (%suffix :initform () :accessor suffix)))
-
-   
