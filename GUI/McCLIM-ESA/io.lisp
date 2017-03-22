e9e83fcef0f9b2a5e14c2944b01ec781ad37f254
Robert Strandh <robert.strandh@gmail.com>
2017-03-22 18:05:51 +0100

    Remove useless class.

--- GUI/McCLIM-ESA/io.lisp
+++ GUI/McCLIM-ESA/io.lisp
@@ -1,10 +1,5 @@
 (cl:in-package #:climacs-esa-gui)
 
-(defclass buffer (esa-buffer:esa-buffer-mixin)
-  ((%cluffer-buffer
-    :initarg :cluffer-buffer
-    :reader cluffer-buffer)))
-
 (defmethod esa-buffer:frame-make-buffer-from-stream ((frame climacs) stream)
   (multiple-value-bind (buffer cursor)
       (climacs2-base:make-empty-standard-buffer-and-cursor)
