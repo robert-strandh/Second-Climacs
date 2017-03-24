276b67ac53ea9fd89404f4658aeb23708126266c
Robert Strandh <robert.strandh@gmail.com>
2017-03-24 07:48:37 +0100

    Eliminate variable X.  Its value is always 0.

--- GUI/McCLIM-ESA/View/Fundamental/view.lisp
+++ GUI/McCLIM-ESA/View/Fundamental/view.lisp
@@ -24,7 +24,6 @@
          (text-width (clim:text-style-width text-style pane))
          (text-ascent (clim:text-style-ascent text-style pane))
          (y (+ text-ascent (* line-number text-height)))
-         (x (* start-column text-width))
          (clim-view (clim:stream-default-view pane))
          (climacs-view (second-climacs-clim-base:climacs-view clim-view))
          (cursor (climacs2-base:cursor climacs-view))
@@ -34,17 +33,17 @@
                (clim:draw-rectangle* pane 1 (- y text-height) 4 y
                                      :ink clim:+blue+)
                (unless (zerop (length contents))
-                 (clim:draw-text* pane contents (+ x 5) y)))
+                 (clim:draw-text* pane contents 5 y)))
               ((= cursor-column-number (length contents))
                (unless (zerop (length contents))
-                 (clim:draw-text* pane contents x y))
+                 (clim:draw-text* pane contents 0 y))
                (let ((cursor-x (* (length contents) text-width)))
                  (clim:draw-rectangle* pane
                                        (1+ cursor-x) (- y text-height)
                                        (+ cursor-x 4) y
                                        :ink clim:+blue+)))
               (t
-               (clim:draw-text* pane contents x y
+               (clim:draw-text* pane contents 0 y
                                 :start 0
                                 :end cursor-column-number)
                (let ((cursor-x (* cursor-column-number text-width)))
@@ -52,6 +51,6 @@
                                        (1+ cursor-x) (- y text-height)
                                        (+ cursor-x 4) y
                                        :ink clim:+blue+)
-                 (clim:draw-text* pane contents (+ x cursor-x 5) y
+                 (clim:draw-text* pane contents (+ cursor-x 5) y
                                   :start cursor-column-number))))
-        (clim:draw-text* pane contents x y))))
+        (clim:draw-text* pane contents 0 y))))
