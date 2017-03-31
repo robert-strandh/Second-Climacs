db9406d01f9db717e77d837e65cff4acfc49457f
Robert Strandh <robert.strandh@gmail.com>
2017-03-31 14:11:03 +0200

    Pass the buffer and the cursor when creating a view.

--- Syntax/Common-Lisp/view.lisp
+++ Syntax/Common-Lisp/view.lisp
@@ -2,16 +2,14 @@
 
 (defclass view (climacs2-base:view) ())
 
-(defmethod initialize-instance :after ((instance view) &key)
-  (multiple-value-bind (buffer cursor)
-      (climacs2-base:make-empty-standard-buffer-and-cursor)
-    (let* ((cache (make-instance 'cache))
-           (analyzer (make-instance 'analyzer
-                       :folio cache
-                       :buffer buffer)))
-      (reinitialize-instance instance
-                             :cursor cursor
-			     :analyzer analyzer))))
+(defmethod initialize-instance :after ((instance view) &key buffer cursor)
+  (let* ((cache (make-instance 'cache))
+         (analyzer (make-instance 'analyzer
+                     :folio cache
+                     :buffer buffer)))
+    (reinitialize-instance instance
+                           :cursor cursor
+                           :analyzer analyzer)))
 
 (defmethod second-climacs-clim-base:view-name ((view view))
   "Common Lisp")
--- Syntax/Fundamental/view.lisp
+++ Syntax/Fundamental/view.lisp
@@ -2,13 +2,11 @@
 
 (defclass view (climacs2-base:view) ())
 
-(defmethod initialize-instance :after ((instance view) &key)
-  (multiple-value-bind (buffer cursor)
-      (climacs2-base:make-empty-standard-buffer-and-cursor)
-    (let ((analyzer (make-instance 'analyzer :buffer buffer)))
-      (reinitialize-instance instance
-                             :cursor cursor
-			     :analyzer analyzer))))
+(defmethod initialize-instance :after ((instance view) &key buffer cursor)
+  (let ((analyzer (make-instance 'analyzer :buffer buffer)))
+    (reinitialize-instance instance
+                           :cursor cursor
+                           :analyzer analyzer)))
 
 (defmethod second-climacs-clim-base:view-name ((view view))
   "Fundamental")
