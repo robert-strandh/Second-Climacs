dbea7f14943c63c65077a352175708640093e3b8
Robert Strandh <robert.strandh@gmail.com>
2018-02-02 11:11:48 +0100

    Add package-local nickname CLIM-BASE for SECOND-CLIMACS-CLIM-BASE.

--- GUI/McCLIM-ESA/View/Common-Lisp/packages.lisp
+++ GUI/McCLIM-ESA/View/Common-Lisp/packages.lisp
@@ -2,5 +2,6 @@
 
 (defpackage #:second-climacs-clim-common-lisp-view
   (:use #:common-lisp)
+  (:local-nicknames (#:clim-base #:second-climacs-clim-base))
   (:export #:move-viewport-to-cursor
            #:move-cursor-to-viewport))
