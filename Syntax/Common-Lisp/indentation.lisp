5697fdf36b71c38e26a9fcf9516ab36510032bfa
Robert Strandh <robert.strandh@gmail.com>
2018-02-03 16:33:59 +0100

    Define functions PROPER-LIST-P and SIMPLE-FORM-P.

--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -5,19 +5,26 @@
 ;;; expression parse results of the children of PARSE-RESULT in the
 ;;; same order.
 
-(defun children-correspond-p (form-children parse-result-children)
-  (if (null form-children)
+(defun children-correspond-p (form parse-result-children)
+  (if (null form)
       (or (null parse-result-children)
           (and (not (typep (car parse-result-children)
                            'expression-parse-result))
-               (children-correspond-p form-children
-                                      (cdr parse-result-children))))
+               (children-correspond-p form (cdr parse-result-children))))
       (and (not (null parse-result-children))
            (if (typep (car parse-result-children)
                       'expression-parse-result)
-               (and (eq (car form-children)
+               (and (eq (car form)
                         (expression (car parse-result-children)))
-                    (children-correspond-p (cdr form-children)
+                    (children-correspond-p (cdr form)
                                            (cdr parse-result-children)))
-               (children-correspond-p form-children
-                                      (cdr parse-result-children))))))
+               (children-correspond-p form (cdr parse-result-children))))))
+
+(defun proper-list-p (object)
+  (not (null (ignore-errors (list-length object)))))
+
+(defun simple-form-p (parse-result)
+  (let ((expression (expression parse-result)))
+    (and (consp expression)
+         (proper-list-p expression)
+         (children-correspond-p expression (children parse-result)))))
