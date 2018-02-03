954aaf4107c61dd90914b2afbab1ba40e2bd2fcc
Robert Strandh <robert.strandh@gmail.com>
2018-02-03 15:52:57 +0100

    Define function for comparing children between a form and a parse result.

--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -1,1 +1,23 @@
 (cl:in-package #:climacs-syntax-common-lisp)
+
+;;; We define a form to be SIMPLE if and only if it is a proper list,
+;;; and if the elements of the list correspond exactly to the
+;;; expression parse results of the children of PARSE-RESULT in the
+;;; same order.
+
+(defun children-correspond-p (form-children parse-result-children)
+  (if (null form-children)
+      (or (null parse-result-children)
+          (and (not (typep (car parse-result-children)
+                           'expression-parse-result))
+               (children-correspond-p form-children
+                                      (cdr parse-result-children))))
+      (and (not (null parse-result-children))
+           (if (typep (car parse-result-children)
+                      'expression-parse-result)
+               (and (eq (car form-children)
+                        (expression (car parse-result-children)))
+                    (children-correspond-p (cdr form-children)
+                                           (cdr parse-result-children)))
+               (children-correspond-p form-children
+                                      (cdr parse-result-children))))))
