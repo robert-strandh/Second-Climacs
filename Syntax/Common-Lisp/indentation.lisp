cf81c48b12a077349f3a6250b360a73bdbe034fc
Robert Strandh <robert.strandh@gmail.com>
2018-02-13 11:33:32 +0100

    Define method on compute-child-indentations specialized to LET.

--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -63,3 +63,22 @@
         (loop for child in remaining-children
               unless (zerop (start-column child))
                 do (setf (indentation child) start-column))))))
+
+(defmethod compute-child-indentations
+    (wad (token (eql (intern-token '#:common-lisp '#:let))) client)
+  (let ((children (children wad)))
+    (if (null children)
+        ;; We have no bindings and no LET body.  Do nothing.
+        nil
+        (let ((start-column (start-column (first children))))
+          (cond ((zerop start-column)
+                 ;; The first argument starts on the same line as the
+                 ;; operator.  Indent every line up to and including
+                 ;; the first expression wad as the first child.
+                 (pop children)
+                 (loop until (or (null children)
+                                 (typep (first children) 'expression-wad))
+                       do (setf (indentation (first children)) start-column)
+                          (pop children))
+                 (unless (null children)
+                   (compute-binding-indentations (car children) client)
