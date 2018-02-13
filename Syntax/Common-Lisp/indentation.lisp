0704c6b048a00afd31a1d2071833b86532cac8bb
Robert Strandh <robert.strandh@gmail.com>
2018-02-13 12:58:59 +0100

    Move default indentation function to support file.

--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -1,32 +1,5 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-;;; This function is called when we have a simple form, and we have no
-;;; particular information about the operator, i.e. either the
-;;; operator is a LAMBDA expression, a symbol that is know to be the
-;;; name of a function, but the lambda list of of that function is
-;;; very simple, or a symbol that we have no information about.
-(defun indent-default-function-call (wad)
-  (let ((children (children wad)))
-    (unless (null (rest children))
-      (let* ((first-child (first children))
-             (first-child-start-column (start-column first-child)))
-        (unless (zerop first-child-start-column)
-          ;; If the start column of the first child is 0, that means
-          ;; that the first child is positioned on the same line as
-          ;; the operator, so we do not modify its indentation.  If it
-          ;; is different from 0, then we set the indentation to be 2
-          ;; columns with respect to the start column of the wad.
-          (setf (indentation first-child) (+ 2 (start-column wad))))
-        (loop for child in (rest children)
-              unless (zerop (start-column child))
-                ;; If the start column of the child is 0, that means
-                ;; that this child is positioned on the same line as
-                ;; the previous child, so we do not modify its
-                ;; indentation.  If it is different from 0, then we
-                ;; set the indentation so that it aligns with the
-                ;; start column of the first child of the wad.
-                do (setf (indentation child) first-child-start-column))))))
-
 ;;; Compute the indentation for a wad representing a single LET or
 ;;; LET* binding.
 (defgeneric compute-binding-indentation (wad client))
