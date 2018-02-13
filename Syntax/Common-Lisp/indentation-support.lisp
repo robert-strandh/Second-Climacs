2c4107e6562e29d366518dd40b0015a019c3090a
Robert Strandh <robert.strandh@gmail.com>
2018-02-13 10:50:25 +0100

    Move support code to component dedicated to that purpose.

--- Syntax/Common-Lisp/indentation-support.lisp
+++ Syntax/Common-Lisp/indentation-support.lisp
@@ -1,5 +1,32 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
+;;; We define a form to be SIMPLE if and only if it is a proper list,
+;;; and if the elements of the list correspond exactly to the
+;;; expression parse results of the children of WAD in the same order.
+
+(defun children-correspond-p (form wad-children)
+  (if (null form)
+      (or (null wad-children)
+          (and (not (typep (car wad-children)
+                           'expression-wad))
+               (children-correspond-p form (cdr wad-children))))
+      (and (not (null wad-children))
+           (if (typep (car wad-children) 'expression-wad)
+               (and (eq (car form)
+                        (expression (car wad-children)))
+                    (children-correspond-p (cdr form)
+                                           (cdr wad-children)))
+               (children-correspond-p form (cdr wad-children))))))
+
+(defun proper-list-p (object)
+  (not (null (ignore-errors (list-length object)))))
+
+(defun simple-form-p (wad)
+  (let ((expression (expression wad)))
+    (and (consp expression)
+         (proper-list-p expression)
+         (children-correspond-p expression (children wad)))))
+
 ;;; To indent the sub-forms of a composite form, we can not use the
 ;;; symbol of the operator of that form, because that symbol may not
 ;;; yet exist, and in fact, it may have a package prefix that is not
--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -1,32 +1,5 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-;;; We define a form to be SIMPLE if and only if it is a proper list,
-;;; and if the elements of the list correspond exactly to the
-;;; expression parse results of the children of WAD in the same order.
-
-(defun children-correspond-p (form wad-children)
-  (if (null form)
-      (or (null wad-children)
-          (and (not (typep (car wad-children)
-                           'expression-wad))
-               (children-correspond-p form (cdr wad-children))))
-      (and (not (null wad-children))
-           (if (typep (car wad-children) 'expression-wad)
-               (and (eq (car form)
-                        (expression (car wad-children)))
-                    (children-correspond-p (cdr form)
-                                           (cdr wad-children)))
-               (children-correspond-p form (cdr wad-children))))))
-
-(defun proper-list-p (object)
-  (not (null (ignore-errors (list-length object)))))
-
-(defun simple-form-p (wad)
-  (let ((expression (expression wad)))
-    (and (consp expression)
-         (proper-list-p expression)
-         (children-correspond-p expression (children wad)))))
-
 ;;; This function is called when we have a simple form, and we have no
 ;;; particular information about the operator, i.e. either the
 ;;; operator is a LAMBDA expression, a symbol that is know to be the
