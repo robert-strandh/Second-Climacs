9e12e72e90920fc5dbcd2bb19518350f52f0d2c9
Robert Strandh <robert.strandh@gmail.com>
2018-02-14 05:16:14 +0100

    Include the bindings in what is passed to the binding indenter.

--- Syntax/Common-Lisp/indentation.lisp
+++ Syntax/Common-Lisp/indentation.lisp
@@ -15,7 +15,7 @@
         ;; We indent every remaining child as the first one, plus 2
         ;; columns.
         (loop for child in remaining-children
-              unless (zerop (start-column child))
+              unless (zerop (start-line child))
                 do (setf (indentation child) (+ start-column 2))
               do (compute-child-indentations child client))))))
 
@@ -35,42 +35,42 @@
         ;; We indent every remaining child as the first one, plus 2
         ;; columns.
         (loop for child in remaining-children
-              unless (zerop (start-column child))
+              unless (zerop (start-line child))
                 do (setf (indentation child) start-column))))))
 
 (defun indent-body (column body-wads client)
   (loop for wad in body-wads
-        unless (zerop (start-column wad))
+        unless (zerop (start-line wad))
           do (setf (indentation wad) column)
         do (compute-child-indentations wad client)))
 
 (defun indent-up-to-and-including-bindings (column wads client)
   (loop for remaining on wads
         for wad = (first remaining)
-        for start-column = (start-column wad)
-        unless (zerop start-column)
+        unless (zerop (start-line wad))
           do (setf (indentation wad) column)
         do (compute-binding-indentations wad client)
         until (typep wad 'expression-wad)
-        finally (return remaining)))
+        finally (return (rest remaining))))
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:let))) client)
-  (let ((children (children wad))
+  (let ((children (rest (children wad)))
         (wad-start-column (start-column wad)))
     (if (null children)
         ;; We have no bindings and no LET body.  Do nothing.
         nil
-        (let ((start-column (start-column (first children))))
-          (if (zerop start-column)
+        (let ((start-line (start-line (first children)))
+              (start-column (start-column (first children))))
+          (if (zerop start-line)
               ;; The first argument starts on the same line as the
               ;; operator.  Indent every line up to and including the
               ;; first expression wad as the first child.
               (let ((body-wads (indent-up-to-and-including-bindings
-                                start-column (rest children) client)))
+                                start-column children client)))
                 (indent-body (+ wad-start-column 2) body-wads client))
               ;; The first argument starts on a different line from
               ;; that of the operator.
               (let ((body-wads (indent-up-to-and-including-bindings
                                 (+ wad-start-column 4) children client)))
-                (indent-body (+ (start-column wad) 2) body-wads client)))))))
+                (indent-body (+ wad-start-column 2) body-wads client)))))))
