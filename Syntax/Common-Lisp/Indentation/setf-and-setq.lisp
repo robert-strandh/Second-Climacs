882bcee2cf8cf11bb8059dbac210b8915691513f
Robert Strandh <robert.strandh@gmail.com>
2018-02-15 10:39:45 +0100

    Fix numerous problems with indentation code for SETF and SETQ.

--- Syntax/Common-Lisp/Indentation/setf-and-setq.lisp
+++ Syntax/Common-Lisp/Indentation/setf-and-setq.lisp
@@ -1,21 +1,48 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
+(defun indent-up-to-and-including-expression (column wads client)
+  (loop for remaining on wads
+        for wad = (first remaining)
+        unless (zerop (start-line wad))
+          do (setf (indentation wad) column)
+        when (typep wad 'expression-wad)
+          do (compute-binding-indentations wad client)
+             (loop-finish)
+        finally (return (rest remaining))))
+
 (defun compute-setf-and-setq-indentation (wad client)
-  (let ((children (children wad)))
-    (unless (null children)
-      (destructuring-bind (first-child . remaining-children) (rest children)
-        (unless (zerop (start-line first-child))
-          (setf (indentation first-child) (+ (start-column wad) 2)))
-        (loop with first-child-column = (start-column first-child)
-              with additional-indentation = 2
-              for child in remaining-children
-              unless (zerop (start-line child))
-                do (setf (indentation child)
-                         (+ first-child-column additional-indentation))
-              when (typep child 'expression-wad)
-                do (setf additional-indentation
-                         (- 2 additional-indentation))
-              do (compute-child-indentations child client))))))
+  (let ((arguments (rest (children wad))))
+    (unless (null arguments)
+      (let* ((first-argument (first arguments))
+             (first-argument-column (start-column first-argument)))
+        (unless (zerop (start-line first-argument))
+          (setf (indentation first-argument)
+                (+ (start-column wad) 2)))
+        (cond ((typep first-argument 'expression-wad)
+               ;; Since the first argument is an EXPRESSION-WAD we
+               ;; need to make sure its children are indented
+               ;; correctly.
+               (compute-child-indentations first-argument client)
+               ;; Keep only arguments beyond the first EXPRESSION-WAD.
+               (pop arguments))
+              (t
+               ;; Indent everything up to and including the first
+               ;; EXPRESSION-WAD.
+               (setf arguments
+                     (indent-up-to-and-including-expression
+                      first-argument-column (rest arguments) client))))
+        ;; When we come here, all arguments up to the first
+        ;; EXPRESSION-WAD have been indented.  If there are remaining
+        ;; arguments, then they start with the first value form.
+        (loop with additional-indentation = 2
+              until (null arguments)
+              do (setf arguments
+                       (indent-up-to-and-including-expression
+                        (+ first-argument-column additional-indentation)
+                        arguments
+                        client))
+                 (setf additional-indentation
+                       (- 2 additional-indentation)))))))
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:setf))) client)
