1bab4e9d55438906cd4edadfc82f970688361110
Robert Strandh <robert.strandh@gmail.com>
2018-02-17 12:52:26 +0100

    Define indentation method specialized to MULTIPLE-VALUE-PROG1.

--- Syntax/Common-Lisp/Indentation/prog1.lisp
+++ Syntax/Common-Lisp/Indentation/prog1.lisp
@@ -46,3 +46,7 @@
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:prog1))) client)
   (indent-prog1-and-multiple-value-prog1 wad client))
+
+(defmethod compute-sub-form-indentations
+    (wad (pawn (eql (intern-pawn '#:common-lisp '#:multiple-value-prog1))) client)
+  (indent-prog1-and-multiple-value-prog1 wad client))
