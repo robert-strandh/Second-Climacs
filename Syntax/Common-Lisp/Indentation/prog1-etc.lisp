802a9b9e9726d6d56b236cfb275a20d5ca804d67
Robert Strandh <robert.strandh@gmail.com>
2018-02-18 09:20:42 +0100

    Simplify PROG1 indentation by calling function for distinguished indentation.

--- Syntax/Common-Lisp/Indentation/prog1-etc.lisp
+++ Syntax/Common-Lisp/Indentation/prog1-etc.lisp
@@ -1,47 +1,12 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
 (defun indent-prog1-etc (wad client)
-  (let ((arguments (rest (children wad))))
-    (unless (null arguments)
-      (let* ((argument (pop arguments))
-             (distinguished-indentation (start-column argument))
-             (body-indentation (+ (start-column wad) 2)))
-        (flet ((maybe-set-indentation (wad-to-indent column)
-                 (unless (or (zerop (start-line wad-to-indent))
-                             (eq wad-to-indent (second (children wad))))
-                   (setf (indentation wad-to-indent) column))))
-          (unless (zerop (start-line argument))
-            (setf (indentation argument) (+ (start-column wad) 4)))
-          (tagbody
-             (if (typep argument 'expression-wad)
-                 (go distinguished-wad)
-                 (go preceding-distinguished))
-           preceding-distinguished
-             ;; ARGUMENT is a NON-EXPRESSION-WAD preceding the list of
-             ;; distinguished.
-             (maybe-set-indentation argument distinguished-indentation)
-             (if (null arguments)
-                 (go out)
-                 (progn (setf argument (pop arguments))
-                        (if (typep argument 'expression-wad)
-                            (go distinguished-wad)
-                            (go preceding-distinguished))))
-           distinguished-wad
-             (maybe-set-indentation argument distinguished-indentation)
-             (compute-child-indentations argument client)
-             (if (null arguments)
-                 (go out)
-                 (progn (setf argument (pop arguments))
-                        (go body-wads)))
-           body-wads
-             (maybe-set-indentation argument body-indentation)
-             (when (typep argument 'expression-wad)
-               (compute-child-indentations argument client))
-             (if (null arguments)
-                 (go out)
-                 (progn (setf argument (pop arguments))
-                        (go body-wads)))
-           out))))))
+  (let* ((fun (lambda (wad) (compute-child-indentations wad client)))
+         (arguments (compute-distinguished-indentation wad 4 fun)))
+    (loop with body-indentation = (+ (start-column wad) 2)
+          for argument in arguments
+          unless (zerop (start-line argument))
+            do (setf (indentation argument) body-indentation))))
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:prog1))) client)
