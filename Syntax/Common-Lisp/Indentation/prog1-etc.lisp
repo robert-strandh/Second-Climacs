85f21801804c91048db0346902975438c0eba768
Robert Strandh <robert.strandh@gmail.com>
2018-02-18 11:13:58 +0100

    Call function INDENT-BODY to do just that.

--- Syntax/Common-Lisp/Indentation/prog1-etc.lisp
+++ Syntax/Common-Lisp/Indentation/prog1-etc.lisp
@@ -3,10 +3,7 @@
 (defun indent-prog1-etc (wad client)
   (let* ((fun (lambda (wad) (compute-child-indentations wad client)))
          (arguments (compute-distinguished-indentation wad 4 fun)))
-    (loop with body-indentation = (+ (start-column wad) 2)
-          for argument in arguments
-          unless (zerop (start-line argument))
-            do (setf (indentation argument) body-indentation))))
+    (indent-body (+ (start-column wad) 2) arguments client)))
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:prog1))) client)
