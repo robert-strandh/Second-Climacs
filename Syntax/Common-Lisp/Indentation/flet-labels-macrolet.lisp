c7e009133ab389662c750a52d604098d36aca4d6
Robert Strandh <robert.strandh@gmail.com>
2018-02-19 11:43:06 +0100

    Protect against malformed definitions.

--- Syntax/Common-Lisp/Indentation/flet-labels-macrolet.lisp
+++ Syntax/Common-Lisp/Indentation/flet-labels-macrolet.lisp
@@ -3,19 +3,23 @@
 (defgeneric indent-local-function-definition (wad client))
 
 (defmethod indent-local-function-definition (wad client)
-  (let* ((fun (lambda (w)
-                (indent-lambda-list w client)))
-         ;; We start by computing the indentation for the the
-         ;; lambda list.
-         (body-wads (compute-distinguished-indentation wad 4 fun)))
-    (indent-body (+ (start-column wad) 2) body-wads client)))
+  (when (and (typep wad 'expression-wad)
+             (consp (children wad)))
+    (let* ((fun (lambda (w)
+                  (indent-lambda-list w client)))
+           ;; We start by computing the indentation for the the
+           ;; lambda list.
+           (body-wads (compute-distinguished-indentation wad 4 fun)))
+      (indent-body (+ (start-column wad) 2) body-wads client))))
 
 (defgeneric indent-local-function-definitions (wad client))
 
 (defmethod indent-local-function-definitions (wad client)
-  (indent-list wad
-               (lambda (w)
-                 (indent-local-function-definition w client))))
+  (when (and (typep wad 'expression-wad)
+             (consp (children wad)))
+    (indent-list wad
+                 (lambda (w)
+                   (indent-local-function-definition w client)))))
 
 (defun indent-flet-etc (wad client)
   (let* ((fun (lambda (w)
