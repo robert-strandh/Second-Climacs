fae65b17fc128b14c89faaf496aa7931e2312775
Robert Strandh <robert.strandh@gmail.com>
2018-03-03 14:03:43 +0100

    Only alternate indentation when there are more than two arguments.

--- Syntax/Common-Lisp/Indentation/setf-and-setq.lisp
+++ Syntax/Common-Lisp/Indentation/setf-and-setq.lisp
@@ -1,25 +1,30 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-;;; FIXME: Treat the case with only two arguments as a special case
-;;; that does not need any additional indentation for the second
-;;; argument.
-
 (defun compute-setf-and-setq-indentation (wad client)
-  (let ((arguments (split-wads (rest (children wad)))))
-    (unless (null arguments)
-      (let* ((first (first (first arguments)))
-             (indentation (if (zerop (start-line first))
-                              (start-column first)
-                              (+ (start-column wad) 2))))
-
-        (loop with additional = 0
-              for argument in arguments
-              do (loop for wad in argument
-                       unless (zerop (start-line wad))
-                         do (setf (indentation wad) (+ indentation additional)))
-                 (when (typep (first (last argument)) 'expression-wad)
-                   (compute-child-indentations (first (last argument)) client))
-                 (setf additional (- 2 additional)))))))
+  (flet ((indent-argument (argument indentation)
+           (loop for wad in argument
+                 unless (zerop (start-line wad))
+                   do (setf (indentation wad) indentation))
+           (when (typep (first (last argument)) 'expression-wad)
+             (compute-child-indentations (first (last argument)) client))))
+    (let ((arguments (split-wads (rest (children wad)))))
+      (unless (null arguments)
+        (let* ((first (first (first arguments)))
+               (indentation (if (zerop (start-line first))
+                                (start-column first)
+                                (+ (start-column wad) 2))))
+          (if (<= (length arguments) 2)
+              ;; When there are only two arguments or fewer, indent
+              ;; all arguments the same.
+              (loop for argument in arguments
+                    do (indent-argument argument indentation))
+              ;; When there are more than two arguments, indent the
+              ;; value arguments a bit more than the variable
+              ;; arguments.
+              (loop with additional = 0
+                    for argument in arguments
+                    do (indent-argument argument (+ indentation additional))
+                       (setf additional (- 2 additional)))))))))
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:setf))) client)
