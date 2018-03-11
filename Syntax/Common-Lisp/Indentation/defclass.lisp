c6e714e6bcec86820b57f8e3ca59491948684e28
Robert Strandh <robert.strandh@gmail.com>
2018-03-11 07:21:40 +0100

    Provide definition of INDENT-SLOTS.

--- Syntax/Common-Lisp/Indentation/defclass.lisp
+++ Syntax/Common-Lisp/Indentation/defclass.lisp
@@ -1,9 +1,17 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-(defun indent-slots (slots client)
-  ;; FIXME: indent slots
-  (declare (ignore slots client))
-  nil)
+;;; SLOTS is a list of wads, some of which may be expression wads and
+;;; some of which may be wads of some other type.  An expression wad
+;;; in the list then represents a single slot specifier.
+(defun indent-slots (wads client)
+  ;; Start by aligning every wad in the list that starts on its own
+  ;; line with the first one.
+  (align-with-first wads)
+  ;; Then, for each expression wad in the list, compute the
+  ;; indentation according to the rules of a single slot specifier.
+  (loop for wad in wads
+        when (typep wad 'expression-wad)
+          do (indent-slot wad client)))
 
 (defun indent-defclass (wad client)
   (let ((arguments (split-wads (rest (children wad)))))
@@ -17,7 +25,10 @@
               (destructuring-bind (slots . options) remaining
                 ;; FIXME: indent options.
                 (declare (ignore options))
-                (indent-slots slots client)))))))))
+                (align-or-indent slots (+ (start-column wad) 2))
+                (let ((last (first (last slots))))
+                  (when (typep last 'expression-wad)
+                    (indent-slots (children last) client)))))))))))
               
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:defclass))) client)
