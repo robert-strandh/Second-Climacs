ac67a8ba438af2fee5d69f8054e857c44e60949c
Robert Strandh <robert.strandh@gmail.com>
2018-03-29 15:30:04 +0200

    Define macro WITH-CURRENT-CURSOR-AND-VIEW.

--- GUI/McCLIM-ESA/Base/with-current-cursor.lisp
+++ GUI/McCLIM-ESA/Base/with-current-cursor.lisp
@@ -8,3 +8,10 @@
 	    (,cursor-var (climacs2-base:cursor ,climacs-view-var)))
        ,@body)))
 
+(defmacro with-current-cursor-and-view ((cursor-var view-var) &body body)
+  (let ((clim-view-var (gensym)))
+    `(let* ((,clim-view-var (clim:stream-default-view (esa:current-window)))
+	    (,view-var (climacs-view ,clim-view-var))
+	    (,cursor-var (climacs2-base:cursor ,view-var)))
+       ,@body)))
+
