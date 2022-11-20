5c4994f021de850ba8c9fbd3cdc14fc47d265f61
Robert Strandh <robert.strandh@gmail.com>
2022-11-20 14:41:21 +0100

    Fix incorrect comment.

--- Base/kill-ring.lisp
+++ Base/kill-ring.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:second-climacs-base)
 
 ;;; For now, the kill ring is an adjustable vector used as a LIFO.  An
-;;; element of the kill ring region of items.  Such a region is also
-;;; represented as an adjustable vector where each element is a line
-;;; of items.  Finally, each line is also an adjustable vector of
+;;; element of the kill ring is a region of items.  Such a region is
+;;; also represented as an adjustable vector where each element is a
+;;; line of items.  Finally, each line is also an adjustable vector of
 ;;; buffer items.
 
 (defparameter *kill-ring* (make-array 0 :adjustable t :fill-pointer t))
