de5527a75b61e569fcbd295e3ed0d07ea8842851
Robert Strandh <robert.strandh@gmail.com>
2022-12-02 14:39:31 +0100

    Remove obsolete comment.

--- Syntax/Common-Lisp/find-wad-containing-position.lisp
+++ Syntax/Common-Lisp/find-wad-containing-position.lisp
@@ -1,20 +1,5 @@
 (cl:in-package #:climacs-syntax-common-lisp)
 
-;;; Given a position in the form of a line number and a column number,
-;;; return the innermost wad that contains that position, or NIL of no
-;;; wad contains the position.  For some wad W, if the line number of
-;;; the position is strictly greater than the absolute start line of W
-;;; and strictly less than the end line of W, then W contains the
-;;; position.  If the line number of the position is the same as the
-;;; start line of W, then the column number must be strictly greater
-;;; than the start column of W (otherwise, the cursor is positioned
-;;; before the wad).  If the line number of the position is the same
-;;; as the end line of W, then the column number must be strictly less
-;;; than the end column of W (otherwise, the cursor is positioned
-;;; after the wad).  Notice that the line number of the position may
-;;; be both the start line and the end line of the wad, in which case
-;;; both the last criteria mentioned must be fulfilled.
-
 ;;; Return true if and only if the position indicated by
 ;;; RELATIVE-LINE-NUMBER and COLUMN-NUMBER is entirely before WAD.  If
 ;;; WAD is an absolute wad, then RELATIVE-LINE-NUMBER must be the
