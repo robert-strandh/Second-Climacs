dfa8564a15985b65d294845ef774ec344bf7c400
Robert Strandh <robert.strandh@gmail.com>
2022-12-06 12:19:05 +0100

    Define command COM-UP-EXPRESSION.

--- GUI/McCLIM-ESA/View/Common-Lisp/common-lisp-command-table.lisp
+++ GUI/McCLIM-ESA/View/Common-Lisp/common-lisp-command-table.lisp
@@ -6,3 +6,21 @@
    clim-base:ascii-insert-table
    clim-base:delete-table
    clim-base:motion-table))
+
+(clim:define-command
+    (com-up-expression :name t :command-table common-lisp-table)
+    ()
+  (let* ((view (clim:stream-default-view (esa:current-window)))
+         (climacs-view (clim-base:climacs-view view))
+         (analyzer (base:analyzer climacs-view))
+         (cache (climacs-syntax-common-lisp:folio analyzer))
+         (climacs-buffer (base:buffer analyzer))
+         (cluffer-buffer (base:cluffer-buffer climacs-buffer)))
+    (climacs-syntax-common-lisp:scavenge cache cluffer-buffer)
+    (climacs-syntax-common-lisp:read-forms analyzer)
+    (second-climacs-clim-base::with-current-cursor (cursor)
+      (climacs-syntax-common-lisp:up-expression cache cursor))))
+
+(esa:set-key `(com-up-expression)
+	     'common-lisp-table
+	     '((#\u :meta :control)))
