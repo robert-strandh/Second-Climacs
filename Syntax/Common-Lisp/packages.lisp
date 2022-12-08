1a0438fe08ac78b94a9a780187f600f9d6194bfc
Robert Strandh <robert.strandh@gmail.com>
2022-12-08 14:37:39 +0100

    Define package-local nickname CLIM-BASE for SECOND-CLIMACS-CLIM-BASE

--- Syntax/Common-Lisp/packages.lisp
+++ Syntax/Common-Lisp/packages.lisp
@@ -5,7 +5,8 @@
   (:shadow #:package-name #:symbol-name)
   (:local-nicknames (#:reader #:eclector.reader)
                     (#:readtable #:eclector.readtable)
-                    (#:base #:second-climacs-base))
+                    (#:base #:second-climacs-base)
+                    (#:clim-base #:second-climacs-clim-base))
   (:export
    #:folio
    #:line-count
