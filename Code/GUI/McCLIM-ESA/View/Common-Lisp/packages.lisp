b27b018c2da2cd599c754a53777c0603cb23e156
Robert Strandh <robert.strandh@gmail.com>
2022-12-08 16:53:01 +0100

    Define PLN CL-SYNTAX for CLIMACS-SYNTAX-COMMON-LISP.

--- Code/GUI/McCLIM-ESA/View/Common-Lisp/packages.lisp
+++ Code/GUI/McCLIM-ESA/View/Common-Lisp/packages.lisp
@@ -3,6 +3,7 @@
 (defpackage #:second-climacs-clim-view-common-lisp
   (:use #:common-lisp)
   (:local-nicknames (#:clim-base #:second-climacs-clim-base)
-                    (#:base #:second-climacs-base))
+                    (#:base #:second-climacs-base)
+                    (#:cl-syntax #:climacs-syntax-common-lisp))
   (:export #:move-viewport-to-cursor
            #:move-cursor-to-viewport))
