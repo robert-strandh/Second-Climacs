fb10740193f5983f72b01d9a0eca537131dd2041
Robert Strandh <robert.strandh@gmail.com>
2022-12-11 08:16:47 +0100

    Define package-local-nickname FUNDAMENTAL-SYNTAX.

--- Code/GUI/McCLIM-ESA/View/Fundamental/packages.lisp
+++ Code/GUI/McCLIM-ESA/View/Fundamental/packages.lisp
@@ -2,6 +2,8 @@
 
 (defpackage #:second-climacs-clim-view-fundamental
   (:use #:common-lisp)
-  (:local-nicknames (#:clim-base #:second-climacs-clim-base)
-                    (#:base #:second-climacs-base))
+  (:local-nicknames
+   (#:clim-base #:second-climacs-clim-base)
+   (#:base #:second-climacs-base)
+   (#:fundamental-syntax #:climacs-syntax-fundamental))
   (:export #:fundamental-view))
