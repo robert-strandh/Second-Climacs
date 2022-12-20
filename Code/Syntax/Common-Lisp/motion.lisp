4851be72d0fddc4d02c44eb19907cc5b7a1d7625
Robert Strandh <robert.strandh@gmail.com>
2022-12-20 15:30:13 +0100

    Define function EXCHANGE-EXPRESSIONS as a basis for the command.

--- Code/Syntax/Common-Lisp/motion.lisp
+++ Code/Syntax/Common-Lisp/motion.lisp
@@ -162,3 +162,84 @@
           (destructuring-bind (new-line-number . wad)
               (first lines-and-wads)
             (backward-non-top-level-expression wad new-line-number cursor))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; This function implements the essence of the command
+;;; EXCHANGE-EXPRESSIONS.
+
+(defun new-cursor ()
+  (make-instance 'cluffer-standard-line:right-sticky-cursor))
+
+(defun copy-cursor-positions (from-cursor to-cursor)
+  (cluffer:attach-cursor
+   to-cursor
+   (cluffer:line from-cursor)
+   (cluffer:cursor-position from-cursor)))
+
+(defun detach-attached-cursors (c1 c2 c3 c4)
+  (flet ((detach-if-attached (cursor)
+           (when (cluffer:cursor-attached-p cursor)
+             (cluffer:detach-cursor cursor))))
+    (detach-if-attached c1)
+    (detach-if-attached c2)
+    (detach-if-attached c3)
+    (detach-if-attached c4)))
+
+;;; FIXME: This function currently does does not work when the cursor
+;;; is inside an atomic expression.
+(defun exchange-expressions (cache cursor)
+  (let ((c1 (new-cursor))
+        (c2 (new-cursor))
+        (c3 (new-cursor))
+        (c4 (new-cursor)))
+    (flet ((ensure-cursors-detached ()
+             (detach-attached-cursors c1 c2 c3 c4)))
+      ;; We first try to position C1 before the preceding expression.
+      (copy-cursor-positions cursor c1)
+      ;; Try to move backward over the preceding expression.  It is
+      ;; possible that this operation might fail if there is no
+      ;; preceding expression.  If that happens, we handle the error,
+      ;; restore everything, and re-signal the condition.
+      (handler-case (backward-expression cache c1)
+        (no-preceding-expression (condition)
+          (ensure-cursors-detached)
+          (error condition)))
+      ;; Next, we try to position C4 at the end of the following
+      ;; expression.
+      (copy-cursor-positions cursor c4)
+      ;; Try to move forward over the next expression.  It is possible
+      ;; that this operation might fail if there is no next
+      ;; expression.  If that happens, we handle the error, restore
+      ;; everything, and re-signal the condition.
+      (handler-case (forward-expression cache c4)
+        (no-following-expression (condition)
+          (ensure-cursors-detached)
+          (error condition)))
+      ;; Come here if everything went well.  Before positioning C2 and
+      ;; C3, we make sure that CURSOR is not exactly at the end of the
+      ;; preceding expression not exactly at the beginning of the
+      ;; following expression.  That way, we can freely insert at
+      ;; those positions without risking that CURSOR will move as
+      ;; well.
+      (base:insert-item cursor #\Space)
+      (base:insert-item cursor #\Space)
+      (base:backward-item cursor)
+      ;; We put C2 at the end of the expression we moved backward
+      ;; over.
+      (copy-cursor-positions c1 c2)
+      (forward-expression cache c2)
+      ;; We put C3 at the beginning of the expression we moved forward
+      ;; over.
+      (copy-cursor-positions c4 c3)
+      (backward-expression cache c3)
+      ;; Swap the contents of the two regions.
+      (let ((preceding-expression (base:move-region-to-vector c1 c2))
+            (following-expression (base:move-region-to-vector c3 c4)))
+        (loop for item across preceding-expression
+              do (base:insert-item c3 item))
+        (loop for item across following-expression
+              do (base:insert-item c1 item)))
+      ;; Remove the additional spaces we inserted.
+      (base:erase-item cursor)
+      (base:delete-item cursor))))
