d41fe75a9b4107c788f1cc99e003fcc7d7cbe7a0
Robert Strandh <robert.strandh@gmail.com>
2022-12-23 15:53:53 +0100

    Define condition KILL-RING-IS-EMPTY.

--- Code/Base/conditions.lisp
+++ Code/Base/conditions.lisp
@@ -8,3 +8,7 @@
   (:report (lambda (condition stream)
              (declare (ignore condition))
              (format stream "The mark is not set"))))
+
+(define-condition kill-ring-is-empty (climacs-error)
+  ()
+  (:report "Kill ring is empty"))
