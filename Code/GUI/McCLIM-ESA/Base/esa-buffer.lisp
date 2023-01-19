61ee0841b4f480a928924cb84fea12c8f20d74a0
Robert Strandh <robert.strandh@gmail.com>
2023-01-19 14:39:04 +0100

    Define :AFTER methods on buffer modifiers to set modify flag.

--- Code/GUI/McCLIM-ESA/Base/esa-buffer.lisp
+++ Code/GUI/McCLIM-ESA/Base/esa-buffer.lisp
@@ -4,3 +4,12 @@
     buffer (esa-buffer:esa-buffer-mixin)
   base:buffer
   ())
+
+(defmethod base:insert-item :after (cursor item)
+  (setf (esa-buffer:needs-saving (base:buffer cursor)) t))
+
+(defmethod base:delete-item :after (cursor)
+  (setf (esa-buffer:needs-saving (base:buffer cursor)) t))
+
+(defmethod base:erase-item :after (cursor)
+  (setf (esa-buffer:needs-saving (base:buffer cursor)) t))
