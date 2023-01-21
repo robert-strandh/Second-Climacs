edc994b03c7fd3fef32e1e1767858c42808f3032
Robert Strandh <robert.strandh@gmail.com>
2023-01-21 10:54:24 +0100

    Define missing function EXCHANGE-CURSOR-POSITIONS.

--- Code/Base/search.lisp
+++ Code/Base/search.lisp
@@ -1,5 +1,15 @@
 (cl:in-package #:second-climacs-base)
 
+(defun exchange-cursor-positions (cursor1 cursor2)
+  (let ((line1 (cluffer:line cursor1))
+        (position1 (cluffer:cursor-position cursor1))
+        (line2 (cluffer:line cursor2))
+        (position2 (cluffer:cursor-position cursor2)))
+    (cluffer:detach-cursor cursor1)
+    (cluffer:detach-cursor cursor2)
+    (cluffer:attach-cursor cursor1 line2 position2)
+    (cluffer:attach-cursor cursor2 line1 position1)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; This function implements the essence of the command
