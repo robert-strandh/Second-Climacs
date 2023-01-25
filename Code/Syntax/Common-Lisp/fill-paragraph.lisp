c6f35001708379de3d3a8f0601603104296500b2
Robert Strandh <robert.strandh@gmail.com>
2023-01-25 17:53:09 +0100

    Extract function FILL-PARAGRAPH-CANDIDATE-P.

--- Code/Syntax/Common-Lisp/fill-paragraph.lisp
+++ Code/Syntax/Common-Lisp/fill-paragraph.lisp
@@ -4,30 +4,31 @@
   ()
   (:report "Not in comment"))
 
+(defun fill-paragraph-candidate-p (wad-descriptor)
+  (and (not (null wad-descriptor))
+       (typep (wad wad-descriptor) 'semicolon-comment-wad)))
+
 (defun fill-paragraph (cache cursor)
   (multiple-value-bind (current parent previous next)
       (compute-wad-descriptors cache cursor)
     (declare (ignore parent))
-    (flet ((candidate-p (wad-descriptor)
-             (and (not (null wad-descriptor))
-                  (typep (wad wad-descriptor) 'semicolon-comment-wad))))
-      (let ((first
-              (if (not (candidate-p current))
-                  (if (not (candidate-p previous))
-                      (if (not (candidate-p next))
-                          (error 'not-in-comment)
-                          next)
-                      (loop for wd = previous then (previous-sibling wd)
-                            while (candidate-p (previous-sibling wd))
-                            finally (return wd)))
-                  (loop for wd = current then (previous-sibling wd)
-                        while (candidate-p (previous-sibling wd))
-                        finally (return wd)))))
-        (let ((wad-descriptors
-                (loop for wd = first then (next-sibling wd)
-                      while (candidate-p wd)
-                      collect wd)))
-          (loop with ignore = (format *trace-output* "~%")
-                for wad-descriptor in wad-descriptors
-                do (format *trace-output* "~s: ~s~%"
-                           wad-descriptor (wad wad-descriptor))))))))
+    (let ((first
+            (if (not (fill-paragraph-candidate-p current))
+                (if (not (fill-paragraph-candidate-p previous))
+                    (if (not (fill-paragraph-candidate-p next))
+                        (error 'not-in-comment)
+                        next)
+                    (loop for wd = previous then (previous-sibling wd)
+                          while (fill-paragraph-candidate-p (previous-sibling wd))
+                          finally (return wd)))
+                (loop for wd = current then (previous-sibling wd)
+                      while (fill-paragraph-candidate-p (previous-sibling wd))
+                      finally (return wd)))))
+      (let ((wad-descriptors
+              (loop for wd = first then (next-sibling wd)
+                    while (fill-paragraph-candidate-p wd)
+                    collect wd)))
+        (loop with ignore = (format *trace-output* "~%")
+              for wad-descriptor in wad-descriptors
+              do (format *trace-output* "~s: ~s~%"
+                         wad-descriptor (wad wad-descriptor)))))))
