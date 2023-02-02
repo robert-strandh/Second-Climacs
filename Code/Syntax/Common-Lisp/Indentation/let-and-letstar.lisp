a4aafb08c4ec28f1a1a6b275845bb8c704c991e1
Robert Strandh <robert.strandh@gmail.com>
2023-02-02 15:15:08 +0100

    Use indentation units to compute indentation of LET bindings.

--- Code/Syntax/Common-Lisp/Indentation/let-and-letstar.lisp
+++ Code/Syntax/Common-Lisp/Indentation/let-and-letstar.lisp
@@ -24,22 +24,16 @@
 (defgeneric compute-binding-indentations (wad client))
 
 (defmethod compute-binding-indentations (wad client)
-  (let ((expression (expression wad)))
-    (when (and (consp expression)
-               (proper-list-p expression))
-      ;; We indent every remaining child as the first one.
-      (let* ((children (children wad))
-             (first-child (first children))
-             (start-column (start-column first-child))
-             (remaining-children (rest children)))
-        (compute-binding-indentation first-child client)
-        ;; We indent every remaining child as the first one, plus 2
-        ;; columns.
-        (loop for child in remaining-children
-              unless (zerop (start-line child))
-                do (setf (indentation child) start-column)
-              when (typep child 'expression-wad)
-                do (compute-binding-indentation child client))))))
+  (unless (null (children wad))
+    (let ((column (start-column (first (children wad))))
+          (indentation-units
+            (compute-indentation-units (children wad))))
+      (loop for indentation-unit in indentation-units
+            do (assign-indentation-of-wads-in-unit
+                indentation-unit column)
+               (loop for wad in indentation-unit
+                     when (typep wad 'expression-wad)
+                       do (compute-binding-indentation wad client))))))
 
 (defun compute-let-and-letstar-indentation (wad client)
   (compute-indentation-single-distinguished
