6b4079ab1e4d3e0f32d7da9a442b82c1ebca0b62
Robert Strandh <robert.strandh@gmail.com>
2023-02-02 13:55:48 +0100

    Complete rewrite of automaton for computing DEFMETHOD indentation.

--- Code/Syntax/Common-Lisp/Indentation/defmethod.lisp
+++ Code/Syntax/Common-Lisp/Indentation/defmethod.lisp
@@ -1,47 +1,67 @@
 (cl:in-package #:second-climacs-syntax-common-lisp)
 
-(defun defmethod-determine-role (wads)
-  (let ((remaining wads))
-    (flet ((skip-no-expression-wads ()
-             (loop until (null remaining)
-                   while (typep (first remaining)
-                                'no-expression-wad)
-                   do (pop remaining))))
+(defun compute-defmethod-indentations (indentation-units client)
+  (let ((indentations (list 1))
+        (current-unit '())
+        (remaining-units indentation-units)
+        (current-wad nil)
+        (seen-expression-wad-p nil))
+    (flet ((next ()
+             (setf current-wad nil)
+             (loop until (typep current-wad 'expression-wad)
+                   do (when (null current-unit)
+                        (setf seen-expression-wad-p nil)
+                        (if (null remaining-units)
+                            (return-from compute-defmethod-indentations
+                              (reverse indentations))
+                            (setf current-unit
+                                  (pop remaining-units))))
+                      (setf current-wad (pop current-unit))))
+           (maybe-assign-indentation (indentation next-default)
+             (unless seen-expression-wad-p
+               (setf seen-expression-wad-p t)
+               (setf (first indentations) indentation)
+               (push next-default indentations))))
       (tagbody
-         (skip-no-expression-wads)
-         ;; The first expression wad is the symbol DEFMETHOD, or else
-         ;; we wouldn't be here.
-         (pop remaining)
-         (skip-no-expression-wads)
-         ;; If it exists, the next wad represents the function name.
-         ;; It is not the purpose of the indentation code to check the
-         ;; validity of that name, so we just skip it.
-         (when (null remaining)
-           (return-from defmethod-determine-role
-             :generic-function-name))
-         (pop remaining)
-         (skip-no-expression-wads)
-         ;; The next wad may represent a method qualifier or the
+         (next)
+         ;; The current wad must be the symbol DEFMETHOD, or else we
+         ;; wouldn't be here.
+         (maybe-assign-indentation 1 6)
+         (next)
+         ;; If it exists, the current wad represents the function
+         ;; name.  It is not the purpose of the indentation code to
+         ;; check the validity of that name, so we just skip it.
+         (maybe-assign-indentation 6 4)
+         (next)
+         ;; The current wad may represent a method qualifier or the
          ;; lambda list.
        method-qualifier-or-lambda-list
-         (when (null remaining)
-           ;; We don't know what it is, so we let the caller decide.
-           (return-from defmethod-determine-role
-             :method-qualifier-or-lambda-list))
-         (let* ((wad (pop remaining)))
-           ;; If this wad represents a list, i.e., it is either a CONS
-           ;; or the symbol NIL, then it is the lambda list.  If not,
-           ;; it is a method qualifier.
-           (unless (or (consp (expression wad))
-                       (wad-represents-symbol-p wad nil))
-             (skip-no-expression-wads)
-             (go method-qualifier-or-lambda-list)))
-         ;; Come here when we have seen a lambda list.  For now, we
-         ;; don't check for declarations and documentation as
-         ;; distinguished from body forms, so if we have seen the
-         ;; lambda list, we are in the body.
-         (return-from defmethod-determine-role
-           :body)))))
+         (unless (or (consp (expression current-wad))
+                     (wad-represents-symbol-p current-wad nil))
+           (maybe-assign-indentation 4 4)
+           (next)
+           (go method-qualifier-or-lambda-list))
+         ;; The current wad is the lambda list.
+         (maybe-assign-indentation 4 2)
+         (indent-lambda-list current-wad client)
+         (next)
+       declaration-or-documentation-or-form
+         (when (and (consp (expression current-wad))
+                    (wad-represents-symbol-p
+                     (first (children current-wad))
+                     'declare))
+           (maybe-assign-indentation 3 2)
+           (next)
+           (go declaration-or-documentation-or-form))
+       documentation-or-form
+         (when (stringp (expression current-wad))
+           (maybe-assign-indentation 3 2)
+           (next))
+       form 
+         (maybe-assign-indentation 2 2)
+         (next)
+         (compute-child-indentations current-wad client)
+         (go form)))))
 
 (defun assign-indentation-of-wads-in-unit
     (indentation-unit indentation)
@@ -54,19 +74,11 @@
 
 (defmethod compute-sub-form-indentations
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:defmethod))) client)
-  (let ((indentation-units (compute-indentation-units (children wad))))
-    ;; This is horribly inefficient.  I am looking for a better
-    ;; technique.
-    (loop with length = (length indentation-units)
-          for indentation-unit in (rest indentation-units)
-          for i from 1 below length
-          for prefix = (subseq indentation-units 0 i)
-          for wads = (apply #'append prefix)
-          for role = (defmethod-determine-role wads)
-          for indentation
-            = (ecase role
-                (:generic-function-name 6)
-                (:method-qualifier-or-lambda-list 4)
-                (:body 2))
+  (let* ((indentation-units
+           (compute-indentation-units (children wad)))
+         (indentations
+           (compute-defmethod-indentations indentation-units client)))
+    (loop for indentation-unit in indentation-units
+          for indentation in indentations
           do (assign-indentation-of-wads-in-unit
               indentation-unit indentation))))
