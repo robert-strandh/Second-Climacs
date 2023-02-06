0c13f8e96a0b773bd27c4e545013168aa2c61e40
Robert Strandh <robert.strandh@gmail.com>
2023-02-06 16:32:39 +0100

    Embryonic code for the DECLARATION declaration specifier.

--- Code/Syntax/Common-Lisp/Indentation/declare.lisp
+++ Code/Syntax/Common-Lisp/Indentation/declare.lisp
@@ -5,3 +5,17 @@
 ;;; forms, it handles declaration specifiers.
 (defgeneric compute-declaration-specifier-argument-indentations
     (wad pawn client))
+
+(defun compute-declaration-indentations (indentation-units client)
+  (declare (ignore indentation-units client)))
+
+(defmethod compute-declaration-specifier-argument-indentations
+    (wad (pawn (eql (intern-pawn '#:common-lisp '#:declaration))) client)
+  (let* ((indentation-units
+           (compute-indentation-units (children wad)))
+         (indentations
+           (compute-declaration-indentations indentation-units client)))
+    (loop for indentation-unit in indentation-units
+          for indentation in indentations
+          do (assign-indentation-of-wads-in-unit
+              indentation-unit indentation))))
