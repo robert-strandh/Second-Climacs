4c3d0dd5962cba1baeede863312921009b6be24a
Robert Strandh <robert.strandh@gmail.com>
2023-02-07 13:38:12 +0100

    Define automaton for the DYNAMIC-EXTENT declaration specifier.

--- Code/Syntax/Common-Lisp/Indentation/declare.lisp
+++ Code/Syntax/Common-Lisp/Indentation/declare.lisp
@@ -24,11 +24,37 @@
      (next)
      (go name)))
 
-(defun compute-declaration-indentations (indentation-units client)
-  (declare (ignore indentation-units client)))
-
 (defmethod compute-declaration-specifier-indentation
     (wad (pawn (eql (intern-pawn '#:common-lisp '#:declaration))) client)
+  (let* ((indentation-units
+           (compute-indentation-units (children wad)))
+         (indentations
+           (compute-declaration-indentations indentation-units client)))
+    (assign-indentation-of-wads-in-units indentation-units indentations)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; DYNAMIC-EXTENT declaration specifier.
+
+(define-indentation-automaton compute-dynamic-extent-indentations
+  (tagbody
+     (next)
+     ;; The current wad must be the symbol DYNAMIC-EXTENT, or else we
+     ;; would not be here.
+     (maybe-assign-indentation 1 3)
+     (next)
+     ;; The current wad represents a name.
+   name-or-function-name
+     ;; We don't bother to recursively compute the indentation of an
+     ;; argument of the form (FUNCTION <fn>), because it would be
+     ;; useful only when <fn> is on a different line from the one
+     ;; FUNCTION is on, and we think that is unlikely.
+     (maybe-assign-indentation 3 3)
+     (next)
+     (go name-or-function-name)))
+
+(defmethod compute-declaration-specifier-indentation
+    (wad (pawn (eql (intern-pawn '#:common-lisp '#:dynamic-extent))) client)
   (let* ((indentation-units
            (compute-indentation-units (children wad)))
          (indentations
