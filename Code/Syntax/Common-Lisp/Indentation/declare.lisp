0b0580c1a1c58bfb016c86fe9212e624f4d33db3
Robert Strandh <robert.strandh@gmail.com>
2023-02-07 14:34:14 +0100

    Define automaton for IGNORE and IGNORABLE declaration specifiers.

--- Code/Syntax/Common-Lisp/Indentation/declare.lisp
+++ Code/Syntax/Common-Lisp/Indentation/declare.lisp
@@ -58,5 +58,42 @@
   (let* ((indentation-units
            (compute-indentation-units (children wad)))
          (indentations
-           (compute-declaration-indentations indentation-units client)))
+           (compute-dynamic-extent-indentations indentation-units client)))
+    (assign-indentation-of-wads-in-units indentation-units indentations)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; IGNORE and IGNORABLE declaration specifiers.
+
+(define-indentation-automaton compute-ignore-indentations
+  (tagbody
+     (next)
+     ;; The current wad must be the symbol IGNORE or the symbol
+     ;; IGNORABLE, or else we would not be here.
+     (maybe-assign-indentation 1 3)
+     (next)
+     ;; The current wad represents a name.
+   name-or-function-name
+     ;; We don't bother to recursively compute the indentation of an
+     ;; argument of the form (FUNCTION <fn>), because it would be
+     ;; useful only when <fn> is on a different line from the one
+     ;; FUNCTION is on, and we think that is unlikely.
+     (maybe-assign-indentation 3 3)
+     (next)
+     (go name-or-function-name)))
+
+(defmethod compute-declaration-specifier-indentation
+    (wad (pawn (eql (intern-pawn '#:common-lisp '#:ignore))) client)
+  (let* ((indentation-units
+           (compute-indentation-units (children wad)))
+         (indentations
+           (compute-ignore-indentations indentation-units client)))
+    (assign-indentation-of-wads-in-units indentation-units indentations)))
+
+(defmethod compute-declaration-specifier-indentation
+    (wad (pawn (eql (intern-pawn '#:common-lisp '#:ignorable))) client)
+  (let* ((indentation-units
+           (compute-indentation-units (children wad)))
+         (indentations
+           (compute-ignore-indentations indentation-units client)))
     (assign-indentation-of-wads-in-units indentation-units indentations)))
