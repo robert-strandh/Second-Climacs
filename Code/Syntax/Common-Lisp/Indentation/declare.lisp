29a5f0d089ea829bc606515a9e87bc30b727f94a
Robert Strandh <robert.strandh@gmail.com>
2023-02-08 06:20:26 +0100

    Fix typo in comment.

--- Code/Syntax/Common-Lisp/Indentation/declare.lisp
+++ Code/Syntax/Common-Lisp/Indentation/declare.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:second-climacs-syntax-common-lisp)
 
 ;;; As usual, we don't really compute the indentation of the
-;;; expression itself, in this case the declaration specifier.  In
-;;; stead, we compute the indentation of the sub-expressions of that
+;;; expression itself, in this case the declaration specifier.
+;;; Instead, we compute the indentation of the sub-expressions of that
 ;;; expression.
 (defgeneric compute-declaration-specifier-indentation
     (wad pawn client))
