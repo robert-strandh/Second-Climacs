89c36dd61e2b93107d7300a92c8c9fb8e05b91e2
Robert Strandh <robert.strandh@gmail.com>
2023-02-08 06:20:38 +0100

    Define generic function COMPUOTE-TYPE-SPECIFIER-INDENTATION.

--- Code/Syntax/Common-Lisp/Indentation/type-specifier.lisp
+++ Code/Syntax/Common-Lisp/Indentation/type-specifier.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:second-climacs-syntax-common-lisp)
+
+;;; As usual, we don't really compute the indentation of the
+;;; expression itself, in this case the type specifier.  Instead, we
+;;; compute the indentation of the sub-expressions of that expression.
+(defgeneric compute-type-specifier-indentation
+    (wad pawn client))
