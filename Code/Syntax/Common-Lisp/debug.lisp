26b79d1510b6c1eabba6fcfa5b3d81cf19649724
Robert Strandh <robert.strandh@gmail.com>
2023-02-24 14:10:53 +0100

    Define function FIRST-TOP-LEVEL-WAD.

--- Code/Syntax/Common-Lisp/debug.lisp
+++ Code/Syntax/Common-Lisp/debug.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:second-climacs-syntax-common-lisp)
 
+;;; Return the first top-level wad, or NIL if there is no top-level
+;;; wad.
+(defun first-top-level-wad (cache)
+  (if (null (prefix cache))
+      (if (null (suffix cache))
+          nil
+          (first (suffix cache)))
+      (first (last (prefix cache)))))
+
 (defun print-wad-tree (root stream)
   (utilities.print-tree:print-tree
    stream root
