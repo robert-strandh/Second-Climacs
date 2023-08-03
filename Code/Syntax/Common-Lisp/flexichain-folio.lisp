3b230f47af33a38e9bdac51678b9005d1ba1bee7
Robert Strandh <robert.strandh@gmail.com>
2023-08-03 12:05:04 +0200

    Remove obsolete methods and obsolete comment.

--- Code/Syntax/Common-Lisp/flexichain-folio.lisp
+++ Code/Syntax/Common-Lisp/flexichain-folio.lisp
@@ -1,17 +1,6 @@
 (cl:in-package #:second-climacs-syntax-common-lisp)
 
-;;; The LINES slot contains a flexichain of lines, where each line
-;;; is a vector of items.
 (defclass flexichain-folio (folio)
   ((%lines :initform (make-instance 'flx:standard-flexichain)
            :initarg :lines
            :reader lines)))
-
-(defmethod line-count ((folio flexichain-folio))
-  (flx:nb-elements (lines folio)))
-
-(defmethod line-length ((folio flexichain-folio) line-number)
-  (length (flx:element* (lines folio) line-number)))
-
-(defmethod item ((folio flexichain-folio) line-number item-number)
-  (aref (flx:element* (lines folio) line-number) item-number))
