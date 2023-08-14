766bab8c156309937490ea18de2e4bcd7c5715ce
Robert Strandh <robert.strandh@gmail.com>
2023-08-14 16:30:37 +0200

    Copy CURSOR-POSITIONS and SET-CURSOR-POSITIONS to new system

--- Code/Syntax/Common-Lisp/Incremental-parsing/utilities.lisp
+++ Code/Syntax/Common-Lisp/Incremental-parsing/utilities.lisp
@@ -5,3 +5,19 @@
 
 (defun punctuationp (character)
   (member character '(#\. #\? #\! #\: #\, #\; #\( #\) #\" #\-)))
+
+;;; Return the line number and the column number of CURSOR as two
+;;; values.
+(defun cursor-positions (cursor)
+  (values (cluffer:line-number cursor)
+          (cluffer:cursor-position cursor)))
+
+;;; Set the line number and the column number of CURSOR.
+(defun set-cursor-positions (cursor line-number column-number)
+  (let ((buffer (cluffer:buffer cursor)))
+    (when (cluffer:cursor-attached-p cursor)
+      (cluffer:detach-cursor cursor))
+    (cluffer:attach-cursor
+      cursor
+      (cluffer:find-line buffer line-number)
+      column-number)))
