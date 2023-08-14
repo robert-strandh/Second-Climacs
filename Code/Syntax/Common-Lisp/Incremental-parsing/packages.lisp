195c2e926f7031a60436f191abe0f3b35cd55cff
Robert Strandh <robert.strandh@gmail.com>
2023-08-14 13:25:52 +0200

    Shadow PACKAGE-NAME and SYMBOL-NAME.

--- Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
+++ Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
@@ -2,6 +2,7 @@
 
 (defpackage #:second-climacs-incremental-parsing
   (:use #:common-lisp)
+  (:shadow #:package-name #:symbol-name)
   (:local-nicknames (#:reader #:eclector.reader)
                     (#:readtable #:eclector.readtable)
                     (#:gs #:trivial-gray-streams)
