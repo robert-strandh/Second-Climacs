0f666be0d7625c9881dac347ff136cd686c86b19
Robert Strandh <robert.strandh@gmail.com>
2023-08-14 13:09:26 +0200

    Introduce package-local prefixes for external packages.

--- Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
+++ Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
@@ -2,4 +2,8 @@
 
 (defpackage #:second-climacs-incremental-parsing
   (:use #:common-lisp)
+  (:local-nicknames (#:reader #:eclector.reader)
+                    (#:readtable #:eclector.readtable)
+                    (#:gs #:trivial-gray-streams)
+                    (#:flx #:flexichain))
   (:export))
