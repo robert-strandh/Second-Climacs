ecd80fa9960a2486e4d9114abe407cc906b4ea21
Robert Strandh <robert.strandh@gmail.com>
2023-08-14 17:09:00 +0200

    Export symbols part of the protocol.

--- Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
+++ Code/Syntax/Common-Lisp/Incremental-parsing/packages.lisp
@@ -7,4 +7,56 @@
                     (#:readtable #:eclector.readtable)
                     (#:gs #:trivial-gray-streams)
                     (#:flx #:flexichain))
-  (:export))
+  (:export
+   #:line-count
+   #:line-contents
+   #:analyzer
+   #:update-cache
+   #:map-wads-and-spaces
+
+   ;; Wads
+   #:wad
+   #:expression-wad
+   #:labeled-object-definition-wad
+   #:labeled-object-reference-wad
+   #:no-expression-wad
+   #:skipped-wad
+   #:comment-wad
+   #:block-command-wad
+   #:semicolon-comment-wad
+   #:word-wad
+   #:misspelled
+   #:ignored-wad
+   #:sharpsign-wad
+   #:sharpsign-plus-wad
+   #:sharpsign-minus-wad
+   #:read-suppress-wad
+   #:reader-macro-wad
+   #:error-wad
+   #:start-line
+   #:height
+   #:end-line
+   #:start-column
+   #:end-column
+   #:min-column-number
+   #:max-column-number
+   #:children
+   #:expression
+   #:token
+
+   ;; Tokens
+   #:token ; class
+   #:symbol-token
+   #:non-existing-package-symbol-token
+   #:non-existing-symbol-token
+   #:existing-symbol-token
+   #:value-token
+   #:numeric-token
+   #:other-token
+   #:package-name
+   #:package-marker-1
+   #:package-marker-2
+
+   #:total-width
+   #:cache
+   #:name))
