ae96a79e419c4345dbbd32b947b935a6db334a49
Robert Strandh <robert.strandh@gmail.com>
2023-08-16 17:23:00 +0200

    No longer export names of items now in Incremental parsing system.

--- Code/Syntax/Common-Lisp/packages.lisp
+++ Code/Syntax/Common-Lisp/packages.lisp
@@ -10,25 +10,13 @@
                     (#:flx #:flexichain)
                     (#:ip #:second-climacs-incremental-parsing))
   (:export
-   #:line-count
    #:line-length
-   #:line-contents
    #:item
    #:common-lisp-view
    #:set-common-lisp-mode
    #:view
-   #:analyzer
-   #:update-cache
    #:wad-to-cst
-   #:map-wads-and-spaces
 
-   #:total-width
-   #:package-name
-   #:package-marker-1
-   #:package-marker-2
-   #:name
-   #:cache
-   #:max-line-width
    #:indent-line
 
    #:up-expression
