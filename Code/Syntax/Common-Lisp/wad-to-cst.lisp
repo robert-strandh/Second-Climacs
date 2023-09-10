3e3c824f45f17f806f235c50a17527d0f7168527
Robert Strandh <robert.strandh@gmail.com>
2023-09-10 10:38:05 +0200

    Supply package prefix and traverse children in reverse order.

--- Code/Syntax/Common-Lisp/wad-to-cst.lisp
+++ Code/Syntax/Common-Lisp/wad-to-cst.lisp
@@ -2,13 +2,13 @@
 
 ;;; WAD has to be an EXPRESSION-WAD.
 (defun wad-to-cst (wad)
-  (if (null (children wad))
+  (if (null (ip:children wad))
       (make-instance 'cst:atom-cst
         :source wad
-        :raw (expression wad))
+        :raw (ip:expression wad))
       (let ((result (cst:cst-from-expression nil)))
-        (loop for child in (children wad)
-              when (typep child 'expression-wad)
+        (loop for child in (reverse (ip:children wad))
+              when (typep child 'ip:expression-wad)
                 do (setf result (cst:cons (wad-to-cst child) result)))
         (reinitialize-instance result :source wad)
         result)))
