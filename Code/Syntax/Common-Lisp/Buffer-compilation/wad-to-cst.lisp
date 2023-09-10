7aaa0e4c164abfc845c2649d545efa37be386363
Robert Strandh <robert.strandh@gmail.com>
2023-09-10 14:35:50 +0200

    Change package for component and change package prefixes.

--- Code/Syntax/Common-Lisp/Buffer-compilation/wad-to-cst.lisp
+++ Code/Syntax/Common-Lisp/Buffer-compilation/wad-to-cst.lisp
@@ -1,12 +1,12 @@
-(cl:in-package #:second-climacs-syntax-common-lisp)
+(cl:in-package #:second-climacs-buffer-compilation)
 
 ;;; WAD has to be an EXPRESSION-WAD.
 (defun wad-to-cst (wad)
-  (if (null (ip:children wad))
-      (cst:cst-from-expression (ip:expression wad) :source wad)
+  (if (null (inc:children wad))
+      (cst:cst-from-expression (inc:expression wad) :source wad)
       (let ((result (cst:cst-from-expression nil)))
-        (loop for child in (reverse (ip:children wad))
-              when (typep child 'ip:expression-wad)
+        (loop for child in (reverse (inc:children wad))
+              when (typep child 'inc:expression-wad)
                 do (setf result (cst:cons (wad-to-cst child) result)))
         (reinitialize-instance result :source wad)
         result)))
