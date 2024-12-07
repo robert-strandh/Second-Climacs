b7da1241199b5595a0dd7dcc7f188907bdd6ea81
Jan Moringen <jan.moringen@posteo.de>
2024-12-07 14:55:01 +0100

    Add COM-SET-FONT-SIZE command to GLOBAL-TABLE

--- Code/GUI/McCLIM-ESA/Base/global-command-table.lisp
+++ Code/GUI/McCLIM-ESA/Base/global-command-table.lisp
@@ -2,3 +2,44 @@
 
 (clim:define-command-table global-table
   :inherit-from (esa:global-esa-table esa-io:esa-io-table debug-table))
+
+;;; Font size commands
+
+(clim:define-command (com-set-font-size :name t :command-table global-table)
+    ((size '(or integer (member :smaller :larger :normal)))
+     &key (window 't :default (esa:current-window)))
+  ;; TODO: this should be tied into a configuration system, of course
+  ;; FIXME: This is so that the command does not signal an error on
+  ;; upstream McCLIM.
+  (let ((set-text-style (fdefinition '(setf clim:pane-text-style))))
+    (unless (null set-text-style)
+      (funcall set-text-style
+               (clim:merge-text-styles
+                (clim:make-text-style nil nil size)
+                (clim:pane-text-style window))
+               window)))
+  ;; HACK: not applied otherwise
+  (let ((medium (clim:sheet-medium window)))
+    (setf (clim:medium-text-style medium) (clim:medium-default-text-style medium))))
+
+(bind-key 'global-table '(#\+ :control) 'com-set-font-size :larger)
+(bind-key 'global-table '(#\- :control) 'com-set-font-size :smaller)
+(bind-key 'global-table '(#\0 :control) 'com-set-font-size :normal)
+
+(clim:define-gesture-name :increase-font-size :pointer-scroll
+  (:wheel-up :control))
+
+(clim:define-presentation-to-command-translator blank-area->increase-font-size
+    (clim:blank-area com-set-font-size global-table
+     :gesture :increase-font-size)
+    (object)
+  '(:larger))
+
+(clim:define-gesture-name :decrease-font-size :pointer-scroll
+  (:wheel-down :control))
+
+(clim:define-presentation-to-command-translator blank-area->decrease-font-size
+    (clim:blank-area com-set-font-size global-table
+     :gesture :decrease-font-size)
+    (object)
+  '(:smaller))
